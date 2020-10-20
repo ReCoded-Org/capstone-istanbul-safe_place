791f0b3692399f8c0ac7e635476a824eeea1444b
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-20 22:39:25 +0300

    change app function to arrow function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import Home from "./containers/Home";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Home />
     </div>
   );
-}
+};
 
 export default App;
