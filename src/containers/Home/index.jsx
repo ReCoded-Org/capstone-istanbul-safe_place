c44882f81c40be9d3bdb1e4932f10b5a9b0ce5f0
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-20 23:08:45 +0300

    rename file

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import SeekHelp from "../../components/SeekHelp";
 
 const Home = () => {
   return (
     <div>
-      <SeekHelp />
+      <p>hello</p>
     </div>
   );
 };
