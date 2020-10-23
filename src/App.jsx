2194d3d7316d0be8b7f218875aa2878d072055c3
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-23 16:35:24 +0300

    add slick-carousel dependency, import bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import Home from "./containers/Home";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
