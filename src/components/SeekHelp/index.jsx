be375910308d1217473e090acc6ecfb2ceafee49
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-23 16:31:58 +0300

    add header to seek help section

--- src/components/SeekHelp/index.jsx
+++ src/components/SeekHelp/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import SeekHelpMap from "../SeekHelpMap";
+import SeekHelpCarousel from "../SeekHelpCarousel";
 
 const SeekHelp = () => {
   return (
     <div>
-      <p>This is Seek Help Page!</p>
+      <h2 style={{ color: "#FE8F7C", fontSize: "28px", textAlign: "center" }}>
+        Find help
+      </h2>
+      <p style={{ color: "#4F4F4F", fontSize: "14px", textAlign: "center" }}>
+        Find immediate access to infomration of nearby shelters, psychologist,
+        lawyers or doctors.
+      </p>
+      <SeekHelpMap />
+      <SeekHelpCarousel />
     </div>
   );
 };
