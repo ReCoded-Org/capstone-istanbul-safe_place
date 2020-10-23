f8034cb7664d259232ace9d87dba13a8c378dd58
Jamil Bailony <jBailony@outlook.com>
2020-10-23 23:39:51 +0300

    Setup: add i18next pakeges and setup

---
+++ src/i18n.config.js
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import Backend from 'i18next-http-backend';
+
+i18n.on('languageChanged', function(lng) {
+  localStorage.setItem("lng", lng);
+})
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    whitelist: ['en', 'ar'],
+    interpolation: {
+      escapeValue: false,
+    },
+    ns: ['translation'],
+    backend: {
+      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`,
+      addPath: `${process.env.PUBLIC_URL}/locales/add/{{lng}}/{{ns}}`,
+    },
+    detection: {
+      order: ['localStorage'],
+      lookupLocalStorage: 'lng',
+      checkWhitelist: true
+    },
+    saveMissing: true,
+    saveMissingTo: 'all',
+  });
+
+export default i18n;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n.config";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </React.Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
