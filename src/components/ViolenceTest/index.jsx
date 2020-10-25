766142e01aa52af25bcf9630d5f043d6a69154cb
Hiba <hiba.mashfij@gmail.com>
2020-10-25 11:39:28 +0300

    changed css to scc and fixed layout

--- src/components/ViolenceTest/index.jsx
+++ src/components/ViolenceTest/index.jsx
@@ -3,24 +3,24 @@ import { Container, Col, Row, Button } from 'react-bootstrap';
 import img1 from "../../images/violence-test-image1.png"
 import img2 from "../../images/violence-test-image2.png"
 import img3 from "../../images/violence-test-image3.png"
-import "./ViolenceTest.css"
+import "./ViolenceTest.scss"
 
 const ViolenceTest = () => {
   return (
     <div className="violence-intro">
-    <Container fluid="md" >
+    <Container >
       <Row className="justify-content-md-center">
-        <Col >
-          <img src={img1} alt="img1"/>
+        <Col lg={6} md={12}>
+          <img src={img1} alt="img1" className="img1"/>
         </Col>
-        <Col  >
+        <Col lg={6} md={12}>
          <Row >
          <Col ><img src={img2} alt="img2" />  </Col>
          <Col ><img src={img3} alt="img3" /> </Col>
         </Row>
         <Row >
         <Col >
-          <h2>What is violance against women?</h2>
+          <h3>What is violance against women?</h3>
           <p>According to the first article of the Declaration on the Prevention of Violence Against Women, adopted by the United Nations General Assembly in 1993; It is the threat or coercion of any kind of gender-based violence or act that results in or is likely to result in physical, sexual and emotional harm to women in the public and private spheres, and arbitrarily restricting freedom.</p>
           <div className="violence-test">
            <h5>Are you in an abusive relationship? <span>Take our test</span></h5>
@@ -31,30 +31,6 @@ const ViolenceTest = () => {
        </Row>
      </Container>
      </div>
-
-
-    // <div className="violence-intro">
-    //   <img src={img1} alt="img1" className="violence-img"/>
-    //   <div className="second-section">
-    //     <div className="violence-images">
-    //     <img src={img2} alt="img2" /> 
-    //      <img src={img3} alt="img3" /> 
-    //      </div>
-    //      <div className="violence-test">
-    //      <h2>What is violance against women?</h2>
-    //       <p>According to the first article of the Declaration on the Prevention of Violence Against Women, adopted by the United Nations General Assembly in 1993; It is the threat or coercion of any kind of gender-based violence or act that results in or is likely to result in physical, sexual and emotional harm to women in the public and private spheres, and arbitrarily restricting freedom.</p>
-    //      <div><h5>Are you in an abusive relationship? <span>Take our test</span></h5>
-    //   <Button> Find out now</Button> </div>
-    //       </div> 
-    //   </div>
-    // </div>
-
- 
-
-
-    
-     
-
   );
 };
 
