c071ce855eeef77cf124d4bea244bc6509b7b930
Jamil Bailony <jbailony@outlook.com>
2020-10-26 21:04:30 +0300

    Add Gender violence section #4 (#16)
    * Style: setup the project to use SASS
    
    * Fix: solve all issues generated by ESlint
    
    * Fix: run yarn fix command to fix the styles
    
    * Fix: change the css file extantion to scss in yarn fix command in the package.json file
    
    * Feat: add gender violence section
    
    * Style: convert button style to mixin
    
    * Style: add responsive style to the component
    
    * Fix: run yarn fix to fix the style
    
    * Style: replace hardcoded breakpoints with bootstrap breakpoints mixins
    
    * Fix: capitalize the alt text in the index.jsx file
    
    * FIx: update the snapshot file
    
    * Fix: solve the warning in compilation phase

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SeekHelp from "../../components/SeekHelp";
+import GenderViolenceMain from "../../components/GenderViolenceMain";
 
 const Home = () => (
   <div>
+    <GenderViolenceMain />
     <SeekHelp />
   </div>
 );
