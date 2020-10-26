9657b8fc57126e0a1cc9b310507410d3c8817b26
Hiba <hiba.mashfij@gmail.com>
2020-10-26 11:57:13 +0300

    adding navbar files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => (
+  <div>
+    <p>This is Seek Help Page!</p>
+  </div>
+);
+
+export default Navbar;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SeekHelp from "../../components/SeekHelp";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => (
   <div>
+    <Navbar />
     <SeekHelp />
   </div>
 );
