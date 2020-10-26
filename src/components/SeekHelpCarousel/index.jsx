02c41206d05d225d05105fde0a7d330794b45148
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-26 21:31:43 +0300

    move data specialist.json to translation.json folder

--- src/components/SeekHelpCarousel/index.jsx
+++ src/components/SeekHelpCarousel/index.jsx
@@ -6,9 +6,10 @@ import phoneSvg from "./images/phone.svg";
 import locationSvg from "./images/map-marker.svg";
 import doctorImg from "./images/doctorImage.svg";
 import "./index.scss";
-import specialistData from "./specialistInfo.json";
+import { useTranslation, I18nextProvider } from "react-i18next";
 
 const SeekHelpCarousel = () => {
+  const { t } = useTranslation();
   const settings = {
     dots: true,
     infinite: true,
@@ -18,54 +19,55 @@ const SeekHelpCarousel = () => {
     nextArrow: <SliderArrow leftOrRight={"right"} />,
     prevArrow: <SliderArrow leftOrRight={"left"} />,
   };
-
   const imagesObj = {
     specialist: doctorImg,
   };
 
   return (
-    <Container className="carousel">
-      <Row>
-        <Col md={{ span: 7, offset: 5 }}>
-          <Slider {...settings}>
-            {specialistData.map((el) => {
-              return (
-                <div className="carouselItem" key={el.name}>
-                  <div className="carouselHeaderAndImage">
-                    <div>
-                      <h3>{el.title}</h3>
-                      <h4>{el.name}</h4>
+    <I18nextProvider>
+      <Container className="carousel">
+        <Row>
+          <Col md={{ span: 7, offset: 5 }}>
+            <Slider {...settings}>
+              {t("home.seekHelpSection", { returnObjects: true }).map((el) => {
+                return (
+                  <div className="carouselItem" key={el.name}>
+                    <div className="carouselHeaderAndImage">
+                      <div>
+                        <h3>{t(el.title)}</h3>
+                        <h4>{t(el.name)}</h4>
+                      </div>
+                      <div>
+                        <img src={imagesObj[el.img]} alt="Specialist" />
+                      </div>
                     </div>
                     <div>
-                      <img src={imagesObj[el.img]} alt="Specialist" />
+                      <div className="contactDetails">
+                        <div>
+                          <img src={phoneSvg} alt="Phone Icon" />
+                        </div>
+                        <div>
+                          <p>{t(el.phoneNumber)}</p>
+                        </div>
+                      </div>
+                      <div className="contactDetails">
+                        <div>
+                          <img src={locationSvg} alt="Location Icon" />
+                        </div>
+                        <div>
+                          <p>{t(el.address)}</p>
+                        </div>
+                      </div>
+                      <p>{t(el.moreInfo)}</p>
                     </div>
                   </div>
-                  <div>
-                    <div className="contactDetails">
-                      <div>
-                        <img src={phoneSvg} alt="Phone Icon" />
-                      </div>
-                      <div>
-                        <p>{el.phoneNumber}</p>
-                      </div>
-                    </div>
-                    <div className="contactDetails">
-                      <div>
-                        <img src={locationSvg} alt="Location Icon" />
-                      </div>
-                      <div>
-                        <p>{el.address}</p>
-                      </div>
-                    </div>
-                    <p>{el.moreInfo}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </Slider>
-        </Col>
-      </Row>
-    </Container>
+                );
+              })}
+            </Slider>
+          </Col>
+        </Row>
+      </Container>
+    </I18nextProvider>
   );
 };
 
