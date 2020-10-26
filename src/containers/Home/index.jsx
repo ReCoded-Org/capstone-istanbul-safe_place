21dfdacfb54cfc69c25ccafcabc0a2bec3250e12
Hiba <hiba.mashfij@gmail.com>
2020-10-26 11:27:05 +0300

    Adding help Navbar

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SeekHelp from "../../components/SeekHelp";
+import HelpNavbar from "../../components/HelpNavbar/HelpNavbar";
 
 const Home = () => (
   <div>
+    <HelpNavbar />
     <SeekHelp />
   </div>
 );
