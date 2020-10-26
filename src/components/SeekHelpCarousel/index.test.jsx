0af2f93450277429de530649417778dfac78974d
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-26 13:05:14 +0300

    delete carousel test

--- src/components/SeekHelpCarousel/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SeekHelpCarousel from "./index";
-import renderer from "react-test-renderer";
-
-it("carousel works", () => {
-  const tree = renderer.create(<SeekHelpCarousel />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
