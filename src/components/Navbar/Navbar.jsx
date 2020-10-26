b79e4e566dff4ff03998f5c32651a21efbb6ca22
Hiba <hiba.mashfij@gmail.com>
2020-10-26 12:34:41 +0300

    added right nav items names and links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import "./Navbar.scss";
 
 const MainNavbar = () => (
-  <Navbar className="help-navbar">
-    <Nav className="mr-auto">
-      <p>Immediate helpline:</p>
-      <p>+1-340-5558-235</p>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+  <Navbar className="main-navbar">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="navbar-right">
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#blog">Blog</Nav.Link>
+      <Nav.Link href="#opportunities">Opportunities</Nav.Link>
+      <Nav.Link href="contact">Contact</Nav.Link>
       <Button variant="outline-primary">Search</Button>
-    </Form>
+      </Nav>
   </Navbar>
 );
 
