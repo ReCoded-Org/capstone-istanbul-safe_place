5c76bb1e15b46193cbabbbdb0676f60d6976475c
Hiba <hiba.mashfij@gmail.com>
2020-10-27 11:58:50 +0300

    created snapshot and passed the test

--- src/components/ViolenceTest/index.test.jsx
+++ src/components/ViolenceTest/index.test.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ViolenceTest from "./index";
 
-it("renders correctly", () => {
+it("renders correctly and matches the snapshot", () => {
   const tree = renderer.create(<ViolenceTest />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
