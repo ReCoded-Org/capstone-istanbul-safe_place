13a5cf764a27bfc8529fc4ce41d18abee0332c0e
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-27 16:19:26 +0300

    added titles data

--- src/components/BlogsCarouselHome/index.jsx
+++ src/components/BlogsCarouselHome/index.jsx
@@ -7,32 +7,85 @@ import "./index.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTranslation, I18nextProvider } from "react-i18next";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function BlogsCarouselHome() {
-    const blogsImgs = [carouselFirstImg, carouselSecondImg, carouselThirdImg, carouselForthImg]
-    const renderSlides = () =>
-        blogsImgs.map((num) => (
-            <div className="photoSize">
-                <img src={num} className="photosCarousel"></img>
-            </div>
-        ));
+  const { t } = useTranslation();
+  const settings = {
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrow: true,
+  };
+  const blogsImgs = [
+    carouselFirstImg,
+    carouselSecondImg,
+    carouselThirdImg,
+    carouselForthImg,
+  ];
+  const slidesData = [
+    {
+      id: 1,
+      title: "Dolorem officiis temporibus.",
+      label: "Healthy Me, Healthy We: Preventing Dating Violence.",
+      img: `${carouselFirstImg}`,
+    },
+    {
+      id: 2,
+      title: ",Officia non provident dolor esse et neque.",
+      label: "Don’t Confuse Abuse With Love.",
+      img: `${carouselSecondImg}`,
+    },
+    {
+      id: 3,
+      title: "Ut recusandae vel vitae molestiae id soluta.",
+      label: "Talking to Your Kids About Sexual Assault.",
+      img: `${carouselThirdImg}`,
+    },
+    {
+      id: 4,
+      title: "Qui vel consequatur recusandae illo repellendus.",
+      label: "International Womens Day",
+      img: `${carouselForthImg}`,
+    },
+  ];
 
-    return (
-        <div className="homeCarousel">
+  //   const renderSlides = () =>
+  //     blogsImgs.map((blogImg) => (
+  //       <div className="photoSize">
+  //         <img src={blogImg} className="photosCarousel"></img>
+  //       </div>
+  //     ));
+
+  return (
+    <I18nextProvider>
+      <Container className="homeCarousel">
+        <Row>
+          <Col>
             <h1 className="title">Our Blogs</h1>
             <h6>Infomration about violence, women and health.</h6>
-            <h6>Read different topics that contribute positivley to the mental health ofwomen.</h6>
-            <Slider className="slider"
-                dots={true}
-                slidesToShow={3}
-                slidesToScroll={3}
-                autoplay={true}
-                autoplaySpeed={3000}
-                arrow={true}
-            >
-                {renderSlides()}
+            <h6>
+              Read different topics that contribute positivley to the mental
+              health ofwomen.
+            </h6>
+            <Slider className="slider" {...settings}>
+              {slidesData.map((slide) => (
+                <div className="slide" key={slide.id}>
+                  {/* <h2 className="slide-title">{slide.title}</h2> */}
+                  <label className="blogLabel">{slide.label}</label>
+                  <img className="blogImage" src={slide.img} />
+                </div>
+              ))}
             </Slider>
-            <h6 className="allBlogsBtn">All blog posts <span>&#8594;</span></h6>
-        </div>
-    );
+            <h6 className="allBlogsBtn">
+              All blog posts <span>&#8594;</span>
+            </h6>
+          </Col>
+        </Row>
+      </Container>
+    </I18nextProvider>
+  );
 }
