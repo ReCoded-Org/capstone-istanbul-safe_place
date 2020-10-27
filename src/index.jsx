d05be0ed8f2fea62b229e329f47de9309895f2d8
Hiba <hiba.mashfij@gmail.com>
2020-10-27 13:58:17 +0300

    deleted extra changed file related to bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n.config";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
