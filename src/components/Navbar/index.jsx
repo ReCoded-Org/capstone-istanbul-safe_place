aa2fe07d295b9d8ee35cf24d7fcff494acf496b2
Hiba <hiba.mashfij@gmail.com>
2020-10-28 11:23:57 +0300

    fixed navbar responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 import "./index.scss";
 import logo from "../../images/Logo.svg";
 
 const MainNavbar = () => (
   <Navbar expand="lg" collapseOnSelect>
-    <Navbar.Brand href="#home">
-      <img src={logo} alt="logo" />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#blog">Blog</Nav.Link>
-        <Nav.Link href="#opportunities">Opportunities</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-        <Nav.Link href="#sign-in">
-          <Button variant="outline-primary">Sign In</Button>
-        </Nav.Link>
-        <Nav.Link href="#sign-up">
-          <Button variant="outline-primary" className="signUpBtn">
-            Sign Up
-          </Button>
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#opportunities">Opportunities</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#sign-in">
+            <Button variant="outline-primary">Sign In</Button>
+          </Nav.Link>
+          <Nav.Link href="#sign-up">
+            <Button variant="outline-primary" className="signUpBtn">
+              Sign Up
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 );
 
