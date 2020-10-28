fa7e0b9327f2b7559802b8b0a249b1bf5b45541f
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-28 19:24:56 +0300

    added the base for jobs and training section

--- src/components/JobsTrainingHome/index.jsx
+++ src/components/JobsTrainingHome/index.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./index.scss";
-import { useTranslation, I18nextProvider } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 export default function JobsTrainingHome() {
-
-    return (
-        <I18nextProvider>
-
+  return (
     <Container>
+      <h1>job/training opportunities</h1>
       <Row>
-        <Col className="left"></Col>
-        <Col className="middle"></Col>
-        <Col className="right"></Col>
+        <Col className="sliderBetweenJobTrainings">
+          <div>
+            <h1>Job Opportunities</h1>
+          </div>
+          <div>
+            <h1>Trainings/Workshops</h1>
+          </div>
+        </Col>
+        <Col className="jobOpportunities">
+          <div className="jobTitle"></div>
+          <div className="jobDescription"></div>
+          <div className="jobAddress"></div>
+          <div className="jobContactsDetails"></div>
+          <div className="jobPostDate"></div>
+        </Col>
+        <Col className="trainings">
+          <div className="trainingTitle"></div>
+          <div className="trainingDescription"></div>
+          <div className="trainingAddress"></div>
+          <div className="trainingContactsDetails"></div>
+          <div className="trainingPostDate"></div>
+        </Col>
       </Row>
     </Container>
-    </I18nextProvider>
   );
 }
