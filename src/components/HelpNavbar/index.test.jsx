3625062bda07caf84b8b15a5d090c73e3b233a4a
Hiba <hiba.mashfij@gmail.com>
2020-10-29 01:08:07 +0300

    removing test

--- src/components/HelpNavbar/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import HelpNavbar from "./index";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<HelpNavbar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
