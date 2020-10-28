d5cfb6ec3378622b5c022fb165785e31f2f2da29
Hiba <hiba.mashfij@gmail.com>
2020-10-28 22:06:38 +0300

    fixed bugs

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,35 +2,35 @@ import React from "react";
 import "./index.scss";
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../../images/Logo-footer.png";
-import twitter from "../../images/icons/twitter.svg";
-import linkedin from "../../images/icons/linkedin.svg";
-import instagram from "../../images/icons/instagram.svg";
-import facebook from "../../images/icons/facebook.svg";
+import twitterIcon from "../../images/icons/twitter.svg";
+import linkedinIcon from "../../images/icons/linkedin.svg";
+import instagramIcon from "../../images/icons/instagram.svg";
+import facebookIcon from "../../images/icons/facebook.svg";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="mainFooter">
       <Container>
         <Row>
           <Col>
-            <img src={logo} alt="logo" width="200px" />
+            <img src={logo} alt="Logo" className="logo" />
           </Col>
-          <Col className="footer-info">
+          <Col className="footerInfo">
             <p>Privacy and Cookies</p>
             <p>Terms and Conditions</p>
             <h6>Follow us on social media:</h6>
-            <div className="social-media">
+            <div>
               <a href="https://www.twitter.com/">
-                <img src={twitter} alt="twitter" width="25px" />
+                <img src={twitterIcon} alt="Twitter icon" />
               </a>
               <a href="https://www.linkedin.com/">
-                <img src={linkedin} alt="twitter" width="25px" />
+                <img src={linkedinIcon} alt="Linkedin icon" />
               </a>
               <a href="https://www.instagram.com/">
-                <img src={instagram} alt="instagram" width="25px" />
+                <img src={instagramIcon} alt="Instagram icon" />
               </a>
               <a href="https://www.facebook.com/">
-                <img src={facebook} alt="facebook" width="25px" />
+                <img src={facebookIcon} alt="Facebook icon" />
               </a>
             </div>
           </Col>
