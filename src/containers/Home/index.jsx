b3fef02a4c0945d54487eb9f0a4e79c0f9ce96e1
hiba-machfej <58190902+hiba-machfej@users.noreply.github.com>
2020-10-30 19:55:11 +0300

    Footer (#22)
    * added footer
    
    * added the social media and fixed styling
    
    * changed names to index
    
    * finished footer
    
    * showed map again
    
    * fixed hover and style
    
    * added test
    
    * added px to width
    
    * fixed bugs
    
    * changed the svg names
    
    * fixed bugs
    
    * Rename linkedinIcon.svg to linkedInIcon.svg
    
    * deleted test
    
    * re-did the test
    
    * added /

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import SeekHelp from "../../components/SeekHelp";
+import Footer from "../../components/Footer";
 import MainNavbar from "../../components/Navbar";
 import ViolenceTest from "../../components/ViolenceTest";
 import GenderViolenceMain from "../../components/GenderViolenceMain";
 
-const Home = () => (
-  <div>
-    <MainNavbar />
-    <GenderViolenceMain />
-    <ViolenceTest />
-    <SeekHelp />
-  </div>
-);
-
+const Home = () => {
+  return (
+    <div>
+      <MainNavbar />
+      <GenderViolenceMain />
+      <ViolenceTest />
+      <SeekHelp />
+      <Footer />
+    </div>
+  );
+};
 export default Home;
