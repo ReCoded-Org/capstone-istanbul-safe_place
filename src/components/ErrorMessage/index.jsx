9e57f89e6ae9d62ee7a9e780e4ce2d68d872ae88
Jamil Bailony <jBailony@outlook.com>
2020-10-30 23:05:50 +0300

    Refactor: add the role attribute to the error message
    * Add the role="alert" attribute to every error message

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -4,7 +4,7 @@ import PatchExclamationFll from '../../images/Patch-exclamation-fll.svg'
 
 export default function ErrorMessage({ message = 'Invalid input value' }) {
     return (
-        <div className="errorMessage">
+        <div className="errorMessage" role="alert">
             <img src={PatchExclamationFll} alt="" width="32" height="32" title="Bootstrap" />
             <p>
                 {message}
