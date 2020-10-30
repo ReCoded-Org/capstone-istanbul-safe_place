613ca16fb5aa6bd9d00c7f0ed5666e926a44a00f
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-30 16:58:05 +0300

    added spaces between block of codes

--- src/components/OpportunitiesCard/index.test.jsx
+++ src/components/OpportunitiesCard/index.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import OpportunitiesCart from "./index";
+import OpportunitiesCard from "./index";
 
 it("renders correctly and matches the snapshot", () => {
   const tree = renderer.create(<OpportunitiesCard />).toJSON();
