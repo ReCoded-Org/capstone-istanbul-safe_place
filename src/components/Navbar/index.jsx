26f923e92bfba4bc38c2133e7a93d122219e2f19
Hiba <hiba.mashfij@gmail.com>
2020-10-30 10:01:00 +0300

    changed url and buttons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./index.scss";
 import logo from "../../images/Logo.svg";
 
 const MainNavbar = () => (
   <Navbar expand="lg" collapseOnSelect>
     <Container>
-      <Navbar.Brand href="#home">
+      <Navbar.Brand href="/home">
         <img src={logo} alt="logo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#blog">Blog</Nav.Link>
-          <Nav.Link href="#opportunities">Opportunities</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <Nav.Link href="#sign-in">
-            <Button variant="outline-primary">Sign In</Button>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/blog">Blog</Nav.Link>
+          <Nav.Link href="/opportunities">Opportunities</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link href="/signin">
+            <button type="button">Sign In</button>
           </Nav.Link>
-          <Nav.Link href="#sign-up">
-            <Button variant="outline-primary" className="signUpBtn">
+          <Nav.Link href="/signup">
+            <button type="button" className="signUpBtn">
               Sign Up
-            </Button>
+            </button>
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
