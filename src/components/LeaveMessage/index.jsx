4695ebf63aadad50f051dc242612948d093ba65f
Jamil Bailony <jBailony@outlook.com>
2020-10-31 02:32:24 +0300

    Fix: solve compile warrning

--- src/components/LeaveMessage/index.jsx
+++ src/components/LeaveMessage/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import LeaveMessageInfo from "../LeaveMessageInfo";
 import LeaveMessageForm from "../LeaveMessageForm";
 import "./index.scss";
