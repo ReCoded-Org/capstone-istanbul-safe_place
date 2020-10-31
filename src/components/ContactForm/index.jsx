2b836ebbf3a122cc5d49b4680377223cf0c77bc8
Jamil Bailony <jBailony@outlook.com>
2020-11-01 00:16:04 +0300

    fix typos

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -4,7 +4,7 @@ import { Col } from "react-bootstrap";
 export default function ContactForm() {
   return (
     <Col md={6} xs={12} className="contactForm">
-      <div>contact form, comming soon ...</div>
+      <div>Contact form, coming soon ...</div>
     </Col>
   );
 }
--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -5,7 +5,7 @@ import { Col } from "react-bootstrap";
 export default function ContactInfo() {
   return (
     <Col className="contactInfo" md={6} xs={12}>
-      <div>Contact info, comming soon ...</div>
+      <div>Contact info, coming soon ...</div>
     </Col>
   );
 }
