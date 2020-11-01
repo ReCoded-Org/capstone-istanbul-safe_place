3e281490236dfd3d090bd8927d35f156b238fc21
Hiba <hiba.mashfij@gmail.com>
2020-11-01 11:49:15 +0300

    Moved main navbar to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HelpNavbar from "./components/HelpNavbar";
+import MainNavbar from "./components/Navbar";
 
 const App = () => (
   <div>
     <HelpNavbar />
+    <MainNavbar />
     <Home />
   </div>
 );
