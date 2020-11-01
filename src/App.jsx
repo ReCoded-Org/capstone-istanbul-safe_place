b985dd4caf601942793374ccd5ed7edacd383087
Hiba <hiba.mashfij@gmail.com>
2020-11-01 12:29:47 +0300

    Moved footer from home container to app container

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HelpNavbar from "./components/HelpNavbar";
+import Footer from "./components/Footer";
 
 const App = () => (
   <div>
     <HelpNavbar />
     <Home />
+    <Footer />
   </div>
 );
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SeekHelp from "../../components/SeekHelp";
-import Footer from "../../components/Footer";
 import MainNavbar from "../../components/Navbar";
 import ViolenceTest from "../../components/ViolenceTest";
 import GenderViolenceMain from "../../components/GenderViolenceMain";
@@ -13,7 +12,6 @@ const Home = () => (
     <ViolenceTest />
     <SeekHelp />
     <BlogsCarouselHome />
-    <Footer />
   </div>
 );
 
