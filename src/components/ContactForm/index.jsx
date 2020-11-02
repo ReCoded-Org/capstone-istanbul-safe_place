c38d38c27b2b4e1e4417c9596040aba3e76f3217
Jamil Bailony <jbailony@outlook.com>
2020-11-02 13:39:20 +0300

    Feat: build the structure of the contact section (#38)
    * Feat: build the structure of the contact section
    
    * fix typos
    
    * Fix: update the snapshote to pass the test unite
    
    * Remove the duplicate container
    
    * update the snapshot test

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+export default function ContactForm() {
+  return (
+    <Col md={6} xs={12} className="contactForm">
+      <div>Contact form, coming soon ...</div>
+    </Col>
+  );
+}
