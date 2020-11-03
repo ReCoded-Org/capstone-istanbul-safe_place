3fd108ebc571525a814d0fa290f2847cda27746c
Jamil Bailony <jBailony@outlook.com>
2020-11-03 22:11:26 +0300

    run yarn fix

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import locationIcon from '../../images/icons/locationIcon.svg'
-import phoneIcon from '../../images/icons/phoneIcon.svg'
-import mailIcon from '../../images/icons/mailIcon.svg'
+import locationIcon from "../../images/icons/locationIcon.svg";
+import phoneIcon from "../../images/icons/phoneIcon.svg";
+import mailIcon from "../../images/icons/mailIcon.svg";
 import "./index.scss";
 
 export default function ContactInfo() {
@@ -16,7 +16,7 @@ export default function ContactInfo() {
         oratione perpetua malo quam ostendis sed quia dolor repellendus
         temporibus autem.
       </p>
-      <Row className="contactMethod">
+      <Row className="contactMethods">
         <Col>
           <img className="contactIcon" src={locationIcon} alt="Location Icon" />
           <p>9094 Bay Meadows Street Conyers, GA 30012</p>
@@ -26,7 +26,7 @@ export default function ContactInfo() {
           <p>+229-955-5388-336 +229-955-5373-360</p>
         </Col>
         <Col>
-          <img className="contactIcon" src={mailIcon} alt="Phone Icon" />
+          <img className="contactIcon" src={mailIcon} alt="Contact Icon" />
           <p>office@mail.com support@mail.com</p>
         </Col>
       </Row>
