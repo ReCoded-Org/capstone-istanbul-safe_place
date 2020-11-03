f6c62f7a4e4c87186601521604875a8af3cbfac5
Jamil Bailony <jBailony@outlook.com>
2020-11-03 22:02:23 +0300

    add the content and structure of the section

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -1,11 +1,35 @@
 /* eslint-disable max-len */
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import locationIcon from '../../images/icons/locationIcon.svg'
+import phoneIcon from '../../images/icons/phoneIcon.svg'
+import mailIcon from '../../images/icons/mailIcon.svg'
+import "./index.scss";
 
 export default function ContactInfo() {
   return (
     <Col className="contactInfo" md={6} xs={12}>
-      <div>Contact info, coming soon ...</div>
+      <h2>Leave us a message</h2>
+      <p>
+        Torquatos nostros? quos tu tam crudelis fuisse, nihil oportere nimium
+        nos causae confidere, sed animo etiam erga nos causae confidere, sed uti
+        oratione perpetua malo quam ostendis sed quia dolor repellendus
+        temporibus autem.
+      </p>
+      <Row className="contactMethod">
+        <Col>
+          <img className="contactIcon" src={locationIcon} alt="Location Icon" />
+          <p>9094 Bay Meadows Street Conyers, GA 30012</p>
+        </Col>
+        <Col>
+          <img className="contactIcon" src={phoneIcon} alt="Phone Icon" />
+          <p>+229-955-5388-336 +229-955-5373-360</p>
+        </Col>
+        <Col>
+          <img className="contactIcon" src={mailIcon} alt="Phone Icon" />
+          <p>office@mail.com support@mail.com</p>
+        </Col>
+      </Row>
     </Col>
   );
 }
