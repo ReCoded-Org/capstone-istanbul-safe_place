06519e61ab4c32cc5ab3bc69ba0df9752bec5767
jennethydyrova <67760504+jennethydyrova@users.noreply.github.com>
2020-11-03 12:33:28 +0300

    Build search bar component (#42)
    * add searchBar component, style it
    
    * fix search bar style
    
    * add snapshot test
    
    * remove unused code
    
    * delete console.log
    
    * fix issues
    
    * add space between component and export
    
    * add mixin, change bootstrap form to input
    
    * update snapshot test
    
    Co-authored-by: Louis Li <louisrli@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import SearchForHelpPage from "./containers/SearchForHelpPage";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,6 +15,7 @@ const App = () => (
     <div>
       <HelpNavbar />
       <MainNavbar />
+      <SearchForHelpPage />
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/containers/SearchForHelpPage/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchBar from "../../components/SearchBar";
+
+const SearchForHelpPage = () => {
+  // TODO(jennethydyrova): Finish implementing search here.
+  const handleSearch = (e, keyword) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <SearchBar handleSearch={handleSearch} />
+    </div>
+  );
+};
+
+export default SearchForHelpPage;
