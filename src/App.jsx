8fef3c37d5cf7d80a4de42a2f1ca4730b497e43a
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-03 23:00:59 +0300

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const App = () => (
         </Route>
         <Route exact path="/seekhelp">
           <SearchForHelpPage />
+        </Route>
         <Route exact path="/test">
           <ViolenceTestPage />
         </Route>
