a55d045e7b42d01dba9aa04962c9976063178055
Jamil Bailony <jbailony@outlook.com>
2020-11-03 23:15:20 +0300

    Update index.jsx
    Convert the first letter of the Icon word to lower case

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -18,15 +18,15 @@ export default function ContactInfo() {
       </p>
       <Row className="contactMethods">
         <Col>
-          <img className="contactIcon" src={locationIcon} alt="Location Icon" />
+          <img className="contactIcon" src={locationIcon} alt="Location icon" />
           <p>9094 Bay Meadows Street Conyers, GA 30012</p>
         </Col>
         <Col>
-          <img className="contactIcon" src={phoneIcon} alt="Phone Icon" />
+          <img className="contactIcon" src={phoneIcon} alt="Phone icon" />
           <p>+229-955-5388-336 +229-955-5373-360</p>
         </Col>
         <Col>
-          <img className="contactIcon" src={mailIcon} alt="Contact Icon" />
+          <img className="contactIcon" src={mailIcon} alt="Contact icon" />
           <p>office@mail.com support@mail.com</p>
         </Col>
       </Row>
