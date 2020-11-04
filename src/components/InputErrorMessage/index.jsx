259c26990e86cc3875bd61b5f9f14ca697ebe62a
Jamil Bailony <jBailony@outlook.com>
2020-11-04 20:38:22 +0300

    correct the name of the exclamation mark icon

--- src/components/InputErrorMessage/index.jsx
+++ src/components/InputErrorMessage/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./index.scss";
-import PatchExclamationFll from "../../images/icons/patchExclamationFll.svg";
+import patchExclamationFill from "../../images/icons/patchExclamationFill.svg";
 
 export default function InputErrorMessage({ message }) {
   return (
     <div className="errorMessage" role="alert">
-      <img src={PatchExclamationFll} alt="Error Logo" width="32" height="32" />
+      <img src={patchExclamationFill} alt="Error Logo" width="32" height="32" />
       <p>{message}</p>
     </div>
   );
