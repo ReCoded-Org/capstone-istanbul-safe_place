13fae22b3de24187b54132c524b5fd9ea4eb5b02
jennethydyrova <67760504+jennethydyrova@users.noreply.github.com>
2020-11-08 14:11:42 +0300

    Search help page layout (#61)
    * add images
    
    * add cards, add overflow
    
    * add card style
    
    * create layout for search for help page
    
    * run fix
    
    * delete console.log
    
    * fix test error
    
    * fix issues
    
    * fix issues
    
    * add translation, lowercase icon
    
    * fix test
    
    * fix typo
    
    * delete import
    
    * add import
    
    * delete import
    
    * add import
    
    * delete svg
    
    * add svg, fix netlify error

--- src/containers/SearchForHelpPage/index.jsx
+++ src/containers/SearchForHelpPage/index.jsx
@@ -1,16 +1,46 @@
 import React from "react";
-import SearchBar from "../../components/SearchBar";
+import SeekHelpMap from "../../components/SeekHelpMap";
+import SeekHelpSingleCard from "../../components/SeekHelpSingleCard";
+import { Container, Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import "./index.scss";
 
 const SearchForHelpPage = () => {
-  // TODO(jennethydyrova): Finish implementing search here.
-  const handleSearch = (e, keyword) => {
-    e.preventDefault();
-  };
+  const { t } = useTranslation();
 
   return (
-    <div>
-      <SearchBar handleSearch={handleSearch} />
-    </div>
+    <Container className="searchHelpPage">
+      <h2>{t("searchHelpPage.header")}</h2>
+      <h6>{t("searchHelpPage.subHeader")}</h6>
+      <Row>
+        <DropdownButton title="Location">
+          {t("searchHelpPage.locations", { returnObjects: true }).map((key) => (
+            <Dropdown.Item eventKey={key}>{t(key)}</Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <DropdownButton title="Type of help">
+          {t("searchHelpPage.specialists", { returnObjects: true }).map(
+            (key) => (
+              <Dropdown.Item eventKey={key}>{t(key)}</Dropdown.Item>
+            )
+          )}
+        </DropdownButton>
+      </Row>
+      <Row>
+        <Col>
+          <div className="seekHelpCards">
+            {t("home.seekHelpSection", { returnObjects: true }).map(
+              (details) => {
+                return <SeekHelpSingleCard {...details} />;
+              }
+            )}
+          </div>
+        </Col>
+        <Col>
+          <SeekHelpMap />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
