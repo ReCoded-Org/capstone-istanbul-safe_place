57bde9ebc971f5a8203372f11a7693e6c374fab6
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-08 18:24:05 +0300

    remove unused code

--- src/components/GenderViolenceMain/index.jsx
+++ src/components/GenderViolenceMain/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import stopGenderViolenceImg from "../../images/home/stop-gender-violence.svg";
 import "./index.scss";
 
