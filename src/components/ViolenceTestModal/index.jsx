d7077762906ef26a0411d42cd8b7e66abb92885d
Hiba <hiba.mashfij@gmail.com>
2020-11-08 23:01:05 +0300

    changed show into isShown

--- src/components/ViolenceTestModal/index.jsx
+++ src/components/ViolenceTestModal/index.jsx
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom";
 import "./index.scss";
 
 const ViolenceTestModal = () => {
-  const [show, setShow] = useState(false);
+  const [isShown, setIsShown] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => setIsShown(false);
+  const handleShow = () => setIsShown(true);
 
   return (
     <Row className="justify-content-md-center violenceModal">
+    
       <Button variant="primary" onClick={handleShow}>
         Result
       </Button>
 
-      <Modal show={show} onHide={handleClose} className="modalCard">
+      <Modal show={isShown} onHide={handleClose} className="modalCard">
         <Modal.Body>
           <button type="button" className="close" onClick={handleClose}>
             <span aria-hidden="true">×</span>
@@ -24,7 +25,7 @@ const ViolenceTestModal = () => {
             <p>
               If you answered YES to ANY or ALL of these questions, you may be
               in an abusive relationship. Please call for information and
-              assistance
+              assistance.
             </p>
             <Button type="button">
               <a href="tel:+1-340-5558-235">340-5558-235</a>
