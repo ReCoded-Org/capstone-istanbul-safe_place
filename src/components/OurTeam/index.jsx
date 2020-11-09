a6ad1d5b67e9a3c0b99c465eae7e7758980b422a
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-09 22:43:47 +0300

    add our-team component, add subheader

---
+++ src/components/OurTeam/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./index.scss";
+
+const OurTeam = () => {
+  return (
+    <Container>
+      <h4>Our team</h4>
+    </Container>
+  );
+};
+
+export default OurTeam;
