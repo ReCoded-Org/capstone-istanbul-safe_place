f97225e8ba7afea8cad108d2b94bff440b4e090c
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-09 23:58:33 +0300

    add team member details

--- public/locales/en/index.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-import Smth from "../../../src/components/OurTeam/images/mohammadJundiSmiling.png";
--- src/components/OurTeam/index.jsx
+++ src/components/OurTeam/index.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 import githubLogo from "../../images/icons/githubLogo.svg";
 import linkedInLogo from "../../images/icons/linkedInLogo.svg";
 import "./index.scss";
 
 const OurTeam = () => {
+  const { t } = useTranslation();
   return (
     <Container className="ourTeam">
       <h4>Our team</h4>
+      {t("aboutPage.ourTeamMemberInfo", { returnObjects: true }).map(
+        (member) => {
+          return (
+            <>
+              <img src={member.memberImg} alt={member.name} />
+              <a href={member.githubLink} target="_blank">
+                <img src={githubLogo} alt="Github icon" />
+              </a>
+              <a href={member.linkedInLink} target="_blank">
+                <img src={linkedInLogo} alt="LinkedIn icon" />
+              </a>
+              <h5>{member.name}</h5>
+              <h6>{member.title}</h6>
+            </>
+          );
+        }
+      )}
     </Container>
   );
 };
