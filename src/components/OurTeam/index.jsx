5e812846cac326413677dae3885de39d287518bd
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-10 15:43:21 +0300

    add info about members

--- src/components/OurTeam/index.jsx
+++ src/components/OurTeam/index.jsx
@@ -3,10 +3,12 @@ import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import githubLogo from "../../images/icons/githubLogo.svg";
 import linkedInLogo from "../../images/icons/linkedInLogo.svg";
+import defaultImg from "../../images/icons/defaultImg.svg";
 import "./index.scss";
 
 const OurTeam = () => {
   const { t } = useTranslation();
+
   return (
     <Container className="ourTeam">
       <h4>Our team</h4>
@@ -14,13 +16,26 @@ const OurTeam = () => {
         (member) => {
           return (
             <>
-              <img src={member.memberImg} alt={member.name} />
-              <a href={member.githubLink} target="_blank">
-                <img src={githubLogo} alt="Github icon" />
-              </a>
-              <a href={member.linkedInLink} target="_blank">
-                <img src={linkedInLogo} alt="LinkedIn icon" />
-              </a>
+              <img src={member.image || defaultImg} alt={member.name} />
+              {member.githubLink && (
+                <a
+                  href={member.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={githubLogo} alt="Github icon" />
+                </a>
+              )}
+              {member.linkedInLink && (
+                <a
+                  href={member.linkedInLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={linkedInLogo} alt="LinkedIn icon" />
+                </a>
+              )}
+
               <h5>{member.name}</h5>
               <h6>{member.title}</h6>
             </>
