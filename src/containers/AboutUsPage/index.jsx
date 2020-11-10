8272fdf70bbfad87ca9bcd7eef246cfd15ec725b
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-10 16:00:13 +0300

    fix typo

--- src/containers/AboutUsPage/index.jsx
+++ src/containers/AboutUsPage/index.jsx
@@ -6,7 +6,7 @@ import "./index.scss";
 const AboutPage = () => {
   const { t } = useTranslation();
 
-  // ToDo(jennethydyrova): Add team member's info and create card for each member
+  // ToDo(jennethydyrova): Add team members' info and create card for each member
   return (
     <Container className="aboutUsPage">
       <Row>
