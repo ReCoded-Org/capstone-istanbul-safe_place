0664f8e743ec7bd1cf21189bd50e8d90d63bf31e
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-13 00:38:07 +0300

    naming imroved, fixed styling, fixed bugs

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -1,48 +1,43 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "../SearchBar";
 import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import "./index.scss";
-const NUM_BLOGS_TO_SHOW = 9;
-const BLOGS_URL =
-  "https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts";
+
+const BLOG_API_URL =
+  "https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=9";
 
 export default function Blogs() {
   const { t } = useTranslation();
   const [blogPosts, setBlogPosts] = useState([]);
+
   const fetchBlogPosts = async () => {
-    const data = await fetch(BLOGS_URL);
+    const data = await fetch(BLOG_API_URL);
     const fetchedBlogPosts = await data.json();
     setBlogPosts(fetchedBlogPosts);
   };
   useEffect(() => {
     fetchBlogPosts();
   }, []);
-  const fetchedBlogPostsArr = blogPosts.slice(0, NUM_BLOGS_TO_SHOW);
   return (
-    <div className="blogsMainContainer">
-      <h2 className="ourBlogTitle">Our Blog</h2>
-      <p>
-        Information about violence, women and health. Read different topics
-        <br />
-        that contribute positively to the mental health of women.
-      </p>
-      <SearchBar />
       <Container>
-        {fetchedBlogPostsArr.map((blogPost) => {
+        {blogPosts.map((blogPost) => {
           return (
             <div className="blogsSection">
               <div className="shadowEffect"></div>
-              <h3 className="blogPostTitle">{t(blogPost.title.rendered)}</h3>
+              <h3
+                className="blogPostTitle"
+                dangerouslySetInnerHTML={{
+                  __html: t(blogPost.title.rendered)
+                }}
+              ></h3>
               <img
                 className="blogPostImg"
-                alt="blog Img"
-                src={blogPost.jetpack_featured_media_url}
+                alt="Blog post cover"
+                src={t(blogPost.jetpack_featured_media_url)}
               />
             </div>
           );
         })}
       </Container>
-    </div>
   );
 }
--- src/containers/BlogPage/index.jsx
+++ src/containers/BlogPage/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Blogs from "../../components/Blogs";
+import SearchBar from "../../components/SearchBar";
+import "./index.scss";
+import { useTranslation } from "react-i18next";
 
 export default function BlogPage() {
+  const { t } = useTranslation();
+
   return (
-    <div>
+    <div className="blogsMainContainer">
+      <h2 className="ourBlogTitle">Our Blog</h2>
+      <p className="ourBlogSubtitle">
+        {t(`Information about violence, women and health. Read different topics
+          that contribute positively to the mental health of women.`)}
+      </p>
+      <SearchBar />
       <Blogs />
     </div>
   );
