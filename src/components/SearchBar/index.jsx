069ef77dca8643daf0f01065fe1b52737fe688a0
Hiba <hiba.mashfij@gmail.com>
2020-11-13 13:46:44 +0300

    tried search

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import searchIcon from "./images/searchIcon.svg";
 import "./index.scss";
 
 // This component takes in a function 'handleSearch' as a props, which handles search based on a keyword provided by user
-const SearchBar = ({ handleSearch }) => {
+
+
+const SearchBar = ({ handleSearch , cards}) => {
+
+const cardsArr = [];
+const cardsTitle= cards.map((card) => {
+  cardsArr.push(card.props.title)
+  cardsArr.push(card.props.description)
+})
+console.log(cardsArr)
   const [keyword, setKeyword] = useState("");
+  const [searchJobsResults, setSearchJobsResults] = React.useState([]);
 
   const handleInputChange = (e) => {
     setKeyword(e.target.value);
   };
+
+
+
+useEffect(() => {
+    const results = cardsArr.filter(person =>
+      person.toLowerCase().includes(keyword)
+    );
+    setSearchJobsResults(results);
+  }, [keyword]);
 
   return (
     <Container>
@@ -18,6 +37,7 @@ const SearchBar = ({ handleSearch }) => {
           <input
             placeholder="Search"
             className="mr-sm-2 searchBarInput"
+            value={keyword}
             onChange={(e) => handleInputChange(e)}
           />
           <img
@@ -31,6 +51,9 @@ const SearchBar = ({ handleSearch }) => {
           />
         </Col>
       </Row>
+      {/* {searchJobsResults.map(item => (
+          <li>{item}</li>
+        ))} */}
     </Container>
   );
 };
