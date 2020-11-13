6c7e2bc42f996f3a5d338f7f295ffcb8601ae8be
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-13 13:50:59 +0300

    chatbot setup

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,40 @@ import SignIn from "./components/SignIn";
 import Footer from "./components/Footer";
 import ViolenceTestPage from "./containers/ViolenceTestPage";
 import AboutUsPage from "./containers/AboutUsPage";
+import ChatBot from "react-simple-chatbot";
+
+const steps = [
+  {
+    id: "1",
+    message: "Hello. Are you in an immidiate danger?",
+    trigger: "2",
+  },
+  {
+    id: "2",
+    options: [
+      { value: 1, label: "Yes", trigger: "3" },
+      { value: 2, label: "No", trigger: "4" },
+      { value: 3, label: "I don't know", trigger: "3" },
+    ],
+  },
+  {
+    id: "3",
+    message: "Please call +902127382625",
+    trigger: "2",
+  },
+  {
+    id: "4",
+    message: "Please go to ..",
+    end: true,
+  },
+];
 
 const App = () => (
   <Router>
     <div>
       <HelpNavbar />
       <MainNavbar />
+      <ChatBot steps={steps} />
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/components/ChatBot/index.jsx
@@ -0,0 +1,21 @@
+// import ChatBot from "react-simple-chatbot";
+
+// const ChatBot = () => {
+//     const steps = [
+//   {
+//     id: '0',
+//     message: 'Welcome to react chatbot!',
+//     trigger: '1',
+//   },
+//   {
+//     id: '1',
+//     message: 'Bye!',
+//     end: true,
+//   },
+// ];
+//     return (
+
+//     )
+// }
+
+// export default ChatBot
