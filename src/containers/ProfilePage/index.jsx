b09bf7e3e3c271b926bb33427121040327c0048f
Jamil Bailony <jBailony@outlook.com>
2020-11-15 00:13:31 +0300

    update the component and refactor it

--- src/containers/ProfilePage/index.jsx
+++ src/containers/ProfilePage/index.jsx
@@ -1,76 +1,31 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import UserDetail from '../../components/UserDetail';
-import UserEmail from '../../components/UserEmail';
-import UserPassword from '../../components/UserPassword';
-import defaultProfileImage from '../../images/default-profile-image.png'
-import './index.scss';
-
-// object mapper to map API props to const props
-const userInfoObj = {
-  portraitURL: "",
-  userDetail: {
-    firstName: "",
-    lastName: "",
-    nationality: "",
-    phoneNumber: [],
-    sex: "",
-    birthday: "",
-    address: {
-      street: "",
-      city: "",
-      state: "",
-      zipCode: "",
-      country: "",
-    },
-  },
-  email: {
-    primary: "",
-    secondary: [],
-  },
-  password: {
-    current: "",
-    newPassword: "",
-    confirmNewPassword: "",
-  },
-};
+import UserDetail from "../../components/UserDetail";
+import UserEmail from "../../components/UserEmail";
+import UserPassword from "../../components/UserPassword";
+import defaultProfileImage from "../../images/default-profile-image.png";
+import "./index.scss";
 
 export default function ProfilePage() {
-  const [user, setUser] = React.useState();
   const [elementSelected, setElementSelected] = React.useState("userDetail");
-
-  const updateUserField = (updatedValues) => {
-    setUser((prevUser) => {
-      return { ...prevUser, ...updatedValues };
-    });
-  };
-
-  React.useEffect(() => {
-    setUser(userInfoObj);
-  });
 
   return (
     <Container fluid="md" className="profileContainer">
       <Row className="profilePortrait">
         <h2>Profile</h2>
         <div className="userPortrait">
-          <img
-            src={user?.portraitURL || defaultProfileImage}
-            alt="User portrait"
-          />
+          <img src={defaultProfileImage} alt="User portrait" />
         </div>
       </Row>
       <Row className="section userDetail">
         <button
           className="sectionTitle"
-          onClick={() => setElementSelected('userDetail')}
+          onClick={() => setElementSelected("userDetail")}
           type="button"
         >
           <i
             className={
-              elementSelected === 'userDetail'
-                ? 'arrow down'
-                : 'arrow right'
+              elementSelected === "userDetail" ? "arrow down" : "arrow right"
             }
           />
           User Details
@@ -78,27 +33,21 @@ export default function ProfilePage() {
         <div
           className={`
             sectionContent 
-            ${elementSelected !== 'userDetail' ? `collapsed` : ``}
+            ${elementSelected !== "userDetail" && `collapsed`}
           `}
         >
-          <UserDetail
-            className="sectionContent"
-            userDetail={user?.userDetail}
-            updateUserField={updateUserField}
-          />
+          <UserDetail />
         </div>
       </Row>
       <Row className="section userEmail">
         <button
           className="sectionTitle"
           type="button"
-          onClick={() => setElementSelected('userEmail')}
+          onClick={() => setElementSelected("userEmail")}
         >
           <i
             className={
-              elementSelected === 'userEmail'
-                ? 'arrow down'
-                : 'arrow right'
+              elementSelected === "userEmail" ? "arrow down" : "arrow right"
             }
           />
           Email Address
@@ -106,26 +55,21 @@ export default function ProfilePage() {
         <div
           className={`
             sectionContent 
-            ${elementSelected !== 'userEmail' ? `collapsed` : ``}
+            ${elementSelected !== "userEmail" && `collapsed`}
           `}
         >
-          <UserEmail
-            email={user?.email}
-            updateUserField={updateUserField}
-          />
+          <UserEmail />
         </div>
       </Row>
       <Row className="section userPassword">
         <button
           className="sectionTitle"
           type="button"
-          onClick={() => setElementSelected('userPassword')}
+          onClick={() => setElementSelected("userPassword")}
         >
           <i
             className={
-              elementSelected === 'userPassword'
-                ? 'arrow down'
-                : 'arrow right'
+              elementSelected === "userPassword" ? "arrow down" : "arrow right"
             }
           />
           Change Password
@@ -133,14 +77,10 @@ export default function ProfilePage() {
         <div
           className={`
             sectionContent 
-            ${elementSelected !== 'userPassword' ? `collapsed` : ``}
+            ${elementSelected !== "userPassword" && `collapsed`}
           `}
         >
-          <UserPassword
-            className="sectionContent"
-            password={user?.password}
-            updateUserField={updateUserField}
-          />
+          <UserPassword />
         </div>
       </Row>
       <Row className="submitBtn">
