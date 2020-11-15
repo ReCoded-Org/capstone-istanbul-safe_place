3e7c7bea768bbfa67e6c0a33c66ba37c0a3c2fbe
Jamil Bailony <jBailony@outlook.com>
2020-11-15 11:35:01 +0300

    refactor the inputErrorMessage component

--- src/components/InputErrorMessage/index.jsx
+++ src/components/InputErrorMessage/index.jsx
@@ -2,20 +2,15 @@ import React from "react";
 import "./index.scss";
 import patchExclamationFill from "../../images/icons/patchExclamationFill.svg";
 
-export default function InputErrorMessage({ errors, name }) {
-  if (errors && errors[name]) {
-    return (
-      <div className="errorMessage" role="alert">
-        <img
-          src={patchExclamationFill}
-          alt="Error Logo"
-          width="32"
-          height="32"
-        />
-        <p>{errors[name].message}</p>
-      </div>
-    );
+export default function InputErrorMessage({ error }) {
+  if (!error) {
+    return null;
   }
 
-  return null;
+  return (
+    <div className="errorMessage" role="alert">
+      <img src={patchExclamationFill} alt="Error logo" />
+      <p>{error.message}</p>
+    </div>
+  );
 }
