767155d85f8a494bc6c4546320c50437bd6e3729
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-15 14:41:03 +0300

    removed translation in Blogs

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-import { useTranslation } from "react-i18next";
 import "./index.scss";
 
 const POSTS_PER_PAGE = 9;
 const BLOG_API_URL = `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=${POSTS_PER_PAGE}`;
 
 export default function Blogs() {
-  const { t } = useTranslation();
   const [blogPosts, setBlogPosts] = useState([]);
 
   const fetchBlogPosts = async () => {
@@ -21,24 +19,26 @@ export default function Blogs() {
 
   return (
     <Container>
-      {blogPosts.map((blogPost) => {
+      {blogPosts.map((blogPost) => 
+      { 
         return (
           <div className="blogsSection">
             <div className="shadowEffect"></div>
             <h3
               className="blogPostTitle"
               dangerouslySetInnerHTML={{
-                __html: t(blogPost.title.rendered),
+                __html: blogPost.title.rendered,
               }}
             ></h3>
             <img
               className="blogPostImg"
               alt="Blog post cover"
-              src={t(blogPost.jetpack_featured_media_url)}
+              src={blogPost.jetpack_featured_media_url}
             />
           </div>
         );
-      })}
+      }
+      )}
     </Container>
   );
 }
