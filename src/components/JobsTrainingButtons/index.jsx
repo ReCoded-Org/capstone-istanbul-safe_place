3a04a97f6d55026243e4425f21bb80e4d7a88673
Hiba <hiba.mashfij@gmail.com>
2020-11-16 22:43:02 +0300

    changed state naming

--- src/components/JobsTrainingButtons/index.jsx
+++ src/components/JobsTrainingButtons/index.jsx
@@ -6,23 +6,27 @@ const BUTTON_NAMES = ["Job Opportunities", "Trainings / Workshops"];
 
 const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
   const { t } = useTranslation();
-  //The selected button is disabled to let the user know which data he is viewing (jobs or training).
-  const [isDisabled, setIsDisabled] = useState(true);
-
-  // If a button is disabled it means it's data is already fetched. So, on the click of the other button this function detects the disabled button and fetches the opposite data.
-  const fetchOpportunityData = () => {
-    if (isDisabled) {
-      setSearchCards([...t(`home.trainings`, { returnObjects: true })]);
-      setCardsArr([...t(`home.trainings`, { returnObjects: true })]);
-    } else {
-      setSearchCards([...t(`home.jobOpportunities`, { returnObjects: true })]);
-      setCardsArr([...t(`home.jobOpportunities`, { returnObjects: true })]);
-    }
-    setIsDisabled(!isDisabled);
-  };
+  // The selected button is disabled to let the user know which data he is viewing (jobs or training).
+  const [isJobsSelected, isTrainingSelected] = useState(true);
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(0);
 
   // This variables maps the two button Names and returns a button for each. One for Jobs and one for training to fetch data accordingly.
   const opportunityListBtns = BUTTON_NAMES.map((name, index) => {
+    // If a button is disabled it means it's data is already fetched. So, on the click of the other button this function detects the disabled button and fetches the opposite data.
+    const fetchOpportunityData = () => {
+      if (isJobsSelected) {
+        setSearchCards([...t(`home.trainings`, { returnObjects: true })]);
+        setCardsArr([...t(`home.trainings`, { returnObjects: true })]);
+      } else {
+        setSearchCards([
+          ...t(`home.jobOpportunities`, { returnObjects: true }),
+        ]);
+        setCardsArr([...t(`home.jobOpportunities`, { returnObjects: true })]);
+      }
+      setSelectedButtonIndex(!selectedButtonIndex);
+      isTrainingSelected(!isJobsSelected);
+    };
+
     return (
       <button
         key={name}
@@ -31,7 +35,7 @@ const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
         onClick={() => {
           fetchOpportunityData();
         }}
-        disabled={index === 0 ? isDisabled : !isDisabled}
+        disabled={index === selectedButtonIndex}
       >
         {name}
       </button>
