ca7b4cd9c23b5da6af0af7ccbb0564a84f78722a
Jamil Bailony <jBailony@outlook.com>
2020-11-16 21:56:12 +0300

    build the EmailControl subcomponent

---
+++ src/components/UserEmail/EmailControl/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Container } from "react-bootstrap";
+
+export default function EmailConrtrol({ userEmail, actions }) {
+  return (
+    <Container fluid className="emailBlock">
+      <div className="emailAddress">
+        <p>
+          <strong>
+            {userEmail.email}
+          </strong>
+        </p>
+      </div>
+      <div className="emailControl">
+        <button
+          type="button"
+          data-toggle="tooltip"
+          data-placement="bottom"
+          title="Remove"
+          onClick={() => actions.removeEmail(userEmail.id)}
+        >
+          &#10005;
+        </button>
+        <button
+          type="button"
+          data-toggle="tooltip"
+          data-placement="bottom"
+          title="Make primary"
+          disabled={userEmail.isPrimary}
+          onClick={() => actions.makeEmailPrimary(userEmail.id)}
+        >
+          &#10003;
+        </button>
+      </div>
+    </Container>
+  )
+}
