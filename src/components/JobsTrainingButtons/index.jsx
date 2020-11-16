80a4108d9bde47b33382c0edf35ee4c0b7415573
Hiba <hiba.mashfij@gmail.com>
2020-11-16 19:49:15 +0300

    moved BUTTON_NAMES under imports

--- src/components/JobsTrainingButtons/index.jsx
+++ src/components/JobsTrainingButtons/index.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "./index.scss";
 
+const BUTTON_NAMES = ["Job Opportunities", "Trainings / Workshops"];
+
 const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
   const { t } = useTranslation();
   //The selected button is disabled to let the user know which data he is viewing (jobs or training).
@@ -19,7 +21,6 @@ const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
     setIsDisabled(!isDisabled);
   };
 
-  const BUTTON_NAMES = ["Job Opportunities", "Trainings / Workshops"];
   // This variables maps the two button Names and returns a button for each. One for Jobs and one for training to fetch data accordingly.
   const opportunityListBtns = BUTTON_NAMES.map((name, index) => {
     return (
