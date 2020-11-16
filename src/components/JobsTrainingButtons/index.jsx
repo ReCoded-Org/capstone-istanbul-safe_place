ad9509265117e0ce22028aa7e2194c0e59e9d7e5
Hiba <hiba.mashfij@gmail.com>
2020-11-16 18:10:11 +0300

    changed buttonName to BUTTON_NAMES and moved common factor in if condition

--- src/components/JobsTrainingButtons/index.jsx
+++ src/components/JobsTrainingButtons/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+
 import "./index.scss";
 
 const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
   const { t } = useTranslation();
-  //The selected button is disabled to let the user know which data he is viewing (jobs or training)
+  //The selected button is disabled to let the user know which data he is viewing (jobs or training).
   const [isDisabled, setIsDisabled] = useState(true);
 
   // If a button is disabled it means it's data is already fetched. So, on the click of the other button this function detects the disabled button and fetches the opposite data.
@@ -12,17 +13,16 @@ const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
     if (isDisabled) {
       setSearchCards([...t(`home.trainings`, { returnObjects: true })]);
       setCardsArr([...t(`home.trainings`, { returnObjects: true })]);
-      setIsDisabled(!isDisabled);
     } else {
       setSearchCards([...t(`home.jobOpportunities`, { returnObjects: true })]);
       setCardsArr([...t(`home.jobOpportunities`, { returnObjects: true })]);
-      setIsDisabled(!isDisabled);
     }
+    setIsDisabled(!isDisabled);
   };
 
-  const buttonNames = ["Job Opportunities", "Trainings / Workshops"];
+  const BUTTON_NAMES = ["Job Opportunities", "Trainings / Workshops"];
   // This variables maps the two button Names and returns a button for each. One for Jobs and one for training to fetch data accordingly.
-  const opportunityListBtns = buttonNames.map((name, index) => {
+  const opportunityListBtns = BUTTON_NAMES.map((name, index) => {
     return (
       <button
         key={name}
