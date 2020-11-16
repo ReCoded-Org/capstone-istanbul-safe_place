81498ada64f1ab44cee43e0c6e5e34c65e1b581a
Hiba <hiba.mashfij@gmail.com>
2020-11-16 18:13:24 +0300

    removed extra empty line

--- src/components/JobsTrainingButtons/index.jsx
+++ src/components/JobsTrainingButtons/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-
 import "./index.scss";
 
 const JobsTrainingButtons = ({ setCardsArr, setSearchCards }) => {
