648cb2ae88defb27488bf1a1d3ecbfa168814a2d
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-17 01:26:57 +0300

    search function added

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import "./index.scss";
+import SearchBar from "../../components/SearchBar";
 
 const POSTS_PER_PAGE = 9;
 const BLOG_API_URL = `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=${POSTS_PER_PAGE}`;
@@ -12,13 +13,37 @@ export default function Blogs() {
     const data = await fetch(BLOG_API_URL);
     const fetchedBlogPosts = await data.json();
     setBlogPosts(fetchedBlogPosts);
+    // console.log(fetchedBlogPosts)
   };
   useEffect(() => {
     fetchBlogPosts();
   }, []);
+  console.log(blogPosts)
+
+
+  // const [searchBlogs, setSearchBlogs] = useState(blogPosts);
+
+  const handleSearch = (e, keyword) => {
+    //console.log(keyword)
+    e.preventDefault();
+
+    const filterPost = blogPosts.filter(blog => {
+      console.log(blog)
+      const blogTitle = blog.title.rendered
+      console.log(blogTitle)
+      const results = blogTitle.toLowerCase().includes(keyword.toLowerCase());
+      console.log(results)
+      return results
+
+    })
+    console.log(filterPost)
+    setBlogPosts(filterPost)
+
+  };
 
   return (
     <Container>
+      <SearchBar handleSearch={handleSearch}/>
       {blogPosts.map((blogPost) => 
       { 
         return (
--- src/containers/BlogPage/index.jsx
+++ src/containers/BlogPage/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Blogs from "../../components/Blogs";
-import SearchBar from "../../components/SearchBar";
+// import SearchBar from "../../components/SearchBar";
 import "./index.scss";
 import { useTranslation } from "react-i18next";
 
 export default function BlogPage() {
   const { t } = useTranslation();
+  // const handleSearch = () =>{
 
+  // }
   return (
     <div className="blogsMainContainer">
       <h2 className="ourBlogTitle">{t("blogPage.title")}</h2>
       <p className="ourBlogSubtitle">{t("blogPage.subtitle")}</p>
-      <SearchBar />
+      {/* <SearchBar handleSearch={handleSearch}/> */}
       <Blogs />
     </div>
   );
