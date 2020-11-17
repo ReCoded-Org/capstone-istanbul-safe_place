63c5c285ea97212cd3da09f004095e04e275a1d9
hiba-machfej <58190902+hiba-machfej@users.noreply.github.com>
2020-11-17 18:56:49 +0300

    Added jobs training buttons component (#80)
    * Added jobs training buttons
    
    * fixed buttons according to search
    
    * fixed button and added comments.
    
    * fixed variable name location
    
    * fixed typo
    
    * changed buttonName to BUTTON_NAMES and moved common factor in if condition
    
    * runned yarn fix
    
    * moved BUTTON_NAMES under imports
    
    * added space
    
    * changed state naming
    
    * fixed bugs
    
    * added translation files
    
    * added newline and fixed spacing

---
+++ src/components/JobsTrainingButtons/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "./index.scss";
+
+const BUTTON_SPECS = [
+  {
+    contentTranslationKey: "home.jobOpportunities",
+    buttonTranslationKey: "jobsTrainingPage.buttons.jobOpportunities",
+  },
+  {
+    contentTranslationKey: "home.trainings",
+    buttonTranslationKey: "jobsTrainingPage.buttons.trainings",
+  },
+];
+
+const JobsTrainingButtons = ({ setCardsArr }) => {
+  const { t } = useTranslation();
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(0);
+
+  const opportunityListBtns = BUTTON_SPECS.map((button, index) => {
+    const opportunityData = [
+      ...t(`${button.contentTranslationKey}`, { returnObjects: true }),
+    ];
+
+    const getOpportunityData = () => {
+      setCardsArr(opportunityData);
+      setSelectedButtonIndex(index);
+    };
+
+    return (
+      <button
+        key={t(button.contentTranslationKey)}
+        type="button"
+        className="opportunityListBtn"
+        onClick={() => {
+          getOpportunityData(index);
+        }}
+        disabled={index === selectedButtonIndex}
+      >
+        {t(button.buttonTranslationKey)}
+      </button>
+    );
+  });
+
+  return <div>{opportunityListBtns}</div>;
+};
+
+export default JobsTrainingButtons;
