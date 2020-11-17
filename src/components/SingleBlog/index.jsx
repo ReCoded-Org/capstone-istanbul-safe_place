e9611ea052194ddc58bc6a04effafff56111062d
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-18 02:31:58 +0300

    added fetch to SingleBlog

--- src/components/SingleBlog/index.jsx
+++ src/components/SingleBlog/index.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./index.scss";
 import { Container, Col } from "react-bootstrap";
 import facebookIcon from "../../images/icons/facebookIcon.svg"
 import twitterIcon from "../../images/icons/twitterIcon.svg"
 import linkedInIcon from "../../images/icons/linkedInIcon.svg"
 import instagramIcon from "../../images/icons/instagramIcon.svg"
-import Link from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 
 export default function SingleBlog(props) {
-  // let params = useParams();
-  // const [blogPosts, setBlogPosts] = useState([])
-  // const allBlogPosts = () => {
-  //   const data = await fetch(
-  //     `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts`
-  //   )
-  //   const posts = await data.json();
-  //   setBlogPosts(posts);
-  // }
+  let params = useParams();
+  console.log(params.id)
+  const [blogPost, setBlogPosts] = useState()
+  const allBlogPosts = async () => {
+    const data = await fetch(
+      `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts/${params.id}`
+    )
+    const posts = await data.json();
+    setBlogPosts(posts);
+  }
+  console.log(blogPost)
+  useEffect(() => {
+    allBlogPosts();
+  }, [])
 
-  // useEffect(() => {
-  //   allBlogPosts();
-  // }, [])
+  if (!blogPost) {
+  return <h1>loading</h1>
+} else {
+  console.log(blogPost.title.rendered)
   return (
     <Container>
       <Col>
@@ -29,45 +35,15 @@ export default function SingleBlog(props) {
       </Col>
       <Col xs={12} md={8}>
         <button className="backToBlogBtn">Back to blog</button>
-        <Link to={`/blog/${props.id}`}>
+        <NavLink to={`/blog/${props.id}`}>
         <img className="blogImg" alt="blog img" src={props.jetpack_featured_media_url}/>
-        </Link>
+        </NavLink>
 
         <title className="singleBlogTitle">Women and health</title>
         <h3 className="author">By SafePlace October 10, 2020</h3>
-        <p className="article">
-          Here’s a question: Are your customers really happy? As a business, you
-          spend time, money, and headaches trying to develop best-in-class
-          products and services. You put on armor to go to battle with your
-          competitors, and you sweat blood and tears to grow your brand. But how
-          do you know that your most valuable asset (i.e., your website) is
-          really meeting customer expectations? The customer experience has
-          overtaken price and product as the #1 brand differentiator.
-          Unfortunately, customers aren’t jumping through hoops to tell you
-          about their experiences. It’s estimated that 1 out of every 26 unhappy
-          customers actually complains. The rest, they just leave… forever. If
-          you want to roll out a best-in-class website that convinces and
-          converts, you need to understand what your customers want. So how do
-          you do it? How do you get customers to tell you their pros and their
-          woes? Let’s talk about hunting Ishmael’s white whale. Here’s how you
-          discover what your customers really think about your website. Why
-          Should You Source Feedback From Your Users? Why should you worry about
-          soliciting feedback? After all, can’t you just wait for customers to
-          tell you? Or, can’t you just use analytics and testing software to
-          determine issues? Here’s the problem: software can’t tell you what
-          customers are actually thinking. Sure! There are obvious problems like
-          speed, broken links, and laggy interfaces. But beyond the tangible
-          lies the very real intangibles, and they’re the driving force behind
-          customer satisfaction. 62% of companies are specifically investing in
-          feedback generation solutions to help them meet the ever-changing
-          needs of the modern customer. Over two-thirds of companies compete on
-          customer experience alone. And the number of businesses investing in
-          game-changing omnichannel experiences has jumped from 20% to 80% over
-          the last five years. 62% of companies are specifically investing in
-          feedback generation solutions to help them meet the ever-changing
-          needs of the modern customer. Over two-thirds of companies compete on
-          customer experience alone.
-        </p>
+        <h4 className="article" dangerouslySetInnerHTML={{
+                __html: blogPost.content.rendered
+              }}></h4>
         <p>Share on social media</p>
         <div className="socialMediaIcons">
           <img className="facebookIcon" src={facebookIcon} alt="facebook"/>
@@ -93,4 +69,5 @@ export default function SingleBlog(props) {
       </Col>
     </Container>
   );
+  }
 }
