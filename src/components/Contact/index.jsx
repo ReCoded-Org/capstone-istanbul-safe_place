14f22b5c6f1e733708028193be481e4973c61057
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-18 19:03:18 +0300

    add modal on submit

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Modal } from "react-bootstrap";
 import ContactInfo from "../ContactInfo";
 import ContactForm from "../ContactForm";
 import "./index.scss";
 
 export default function LeaveMessage() {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClose = () => setIsShown(false);
+  const handleShow = () => setIsShown(true);
+
   return (
     <section className="contactSection">
       <Container fluid="md" className="contactContainer">
         <Row>
           <ContactInfo />
-          <ContactForm />
+          <ContactForm handleShow={handleShow} />
+          <Modal show={isShown} onHide={handleClose}>
+            <Modal.Body>
+              <button type="button" className="close" onClick={handleClose}>
+                <span aria-hidden="true">×</span>
+              </button>
+              <div className="contactFormModalBody">
+                <p>Thank you for leaving us your message!</p>
+              </div>
+            </Modal.Body>
+          </Modal>
         </Row>
       </Container>
     </section>
