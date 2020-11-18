e5d5cf0fc1b7f45c16f0d663590c09cbf19e82d5
Jamil Bailony <jBailony@outlook.com>
2020-11-18 15:15:30 +0300

    add comment to Input function

--- src/components/form/Input/index.jsx
+++ src/components/form/Input/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * custom version of the input tag
+ * @param {string} name - input name and id
+ * @param {string} type - input type (text, email, ...etc)
+ * @param {string} placeholder - input placeholder
+ * @param {string} ref - React ref to use with register() form function or any ref
+ * @param {string} label - (optional) if exist, label tag will be added with the name givien
+ */
 export default function Input({ name, type, placeholder, ref, label }) {
   if (!label) {
     return (
