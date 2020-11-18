6d6eed8d97f3048073c14d5ecc5c50e84895d2b5
Jamil Bailony <jBailony@outlook.com>
2020-11-19 01:56:07 +0300

    add translation

--- src/components/Navbar/UserSetting/index.jsx
+++ src/components/Navbar/UserSetting/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Dropdown } from "react-bootstrap";
 import defaultProfileImage from "../../../images/defaultProfileImage.png";
 import firebase from "../../../firebaseConfig";
 import "./index.scss";
 
 export default function UserSetting({ currentUser }) {
+  const { t } = useTranslation();
+
   const handleSignOut = () => {
     firebase.auth().signOut();
   };
@@ -20,13 +23,17 @@ export default function UserSetting({ currentUser }) {
         >
           <img
             src={currentUser.photoURL || defaultProfileImage}
-            alt="User icon"
+            alt={t("navbar.userSettings.userIcon")}
           />
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="/profile">Your profile</Dropdown.Item>
-          <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
+          <Dropdown.Item href="/profile">
+            {t("navbar.userSettings.profile")}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={handleSignOut}>
+            {t("navbar.userSettings.signOut")}
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
