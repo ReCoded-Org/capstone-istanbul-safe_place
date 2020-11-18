f11f21286a6275163c7ce7eeeded8fc49036c66d
Jamil Bailony <jBailony@outlook.com>
2020-11-18 23:22:21 +0300

    add documentation

--- src/auth/PrivateRoute.jsx
+++ src/auth/PrivateRoute.jsx
@@ -2,6 +2,12 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./Authentication";
 
+/**
+ * Use this higher-order component with the component that
+ * require the user to be authentecated. It's used with instead
+ * of <Route />
+ * 
+ */
 const PrivateRoute = ({ children, ...rest }) => {
   const { currentUser } = useContext(AuthContext);
   return (
