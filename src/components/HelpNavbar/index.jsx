05e277465a3fea35811b6b24f09318d280479636
Hiba <hiba.mashfij@gmail.com>
2020-11-19 12:29:40 +0300

    added translation

--- src/components/HelpNavbar/index.jsx
+++ src/components/HelpNavbar/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Navbar, Container, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 import phoneIcon from "../../images/icons/phone.svg";
 import "./index.scss";
 
-const HelpNavbar = () => (
-  <Navbar className="helpNavbar">
-    <Container>
-      <Col>
-        <p>Immediate helpline:</p>
-        <img src={phoneIcon} alt="Phone icon" />
-        <p>+1-340-5558-235</p>
-      </Col>
-    </Container>
-  </Navbar>
-);
+const HelpNavbar = () => {
+  const { t } = useTranslation();
+  return (
+    <Navbar className="helpNavbar">
+      <Container>
+        <Col>
+          <p>{t("helpNavbar.title")}</p>
+          <img src={phoneIcon} alt="Phone icon" />
+          <a href="tel:+1-340-5558-235">{t("helpNavbar.phoneNum")}</a>
+        </Col>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default HelpNavbar;
