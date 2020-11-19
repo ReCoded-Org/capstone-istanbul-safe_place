af5f6b6a93c78ce339453238e613248a48b85005
Jamil Bailony <jBailony@outlook.com>
2020-11-20 01:40:32 +0300

    rename form helpers and add global object schema

---
+++ src/utils/formHelpers.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export function getErrorClass(error) {
+  if (!error) return "";
+
+  return "inputError";
+}
+
+export const formSchemaObject = {
+  email: Joi.string().required().email({ tlds: {} }).messages({
+    "string.empty": `Please provide your email`,
+    "string.email": `Please provide a valid email`,
+  }),
+  stringRequired: Joi.string().required(),
+  anyOptional: Joi.any().optional(),
+}
