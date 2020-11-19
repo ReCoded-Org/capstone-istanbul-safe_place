86f5a61145d96f0a625c89170106bc4c1cbfd8a2
Hiba <hiba.mashfij@gmail.com>
2020-11-19 13:29:24 +0300

    removed commented out code

--- src/containers/BlogPage/index.jsx
+++ src/containers/BlogPage/index.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import Blogs from "../../components/Blogs";
-// import SearchBar from "../../components/SearchBar";
 import "./index.scss";
 import { useTranslation } from "react-i18next";
 
 export default function BlogPage() {
   const { t } = useTranslation();
-  // const handleSearch = () =>{
 
-  // }
   return (
     <div className="blogsMainContainer">
       <h2 className="ourBlogTitle">{t("blogPage.title")}</h2>
       <p className="ourBlogSubtitle">{t("blogPage.subtitle")}</p>
-      {/* <SearchBar handleSearch={handleSearch}/> */}
       <Blogs />
     </div>
   );
