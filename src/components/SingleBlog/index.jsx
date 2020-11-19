4b9045ba608eaeaa6eeb838c100e78b08ff31561
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-20 00:22:00 +0300

    removed coments, fixed style

--- src/components/SingleBlog/index.jsx
+++ src/components/SingleBlog/index.jsx
@@ -9,7 +9,6 @@ import { ReactComponent as InstagramIcon } from "../../images/icons/instagramIco
 
 export default function SingleBlog(props) {
   const params = useParams();
-  // console.log(props);
   const [blogPost, setBlogPosts] = useState();
   const allBlogPosts = async () => {
     const data = await fetch(
@@ -18,7 +17,6 @@ export default function SingleBlog(props) {
     const posts = await data.json();
     setBlogPosts(posts);
   };
-  // console.log(blogPost);
   useEffect(() => {
     allBlogPosts();
   }, []);
@@ -26,23 +24,15 @@ export default function SingleBlog(props) {
   if (!blogPost) {
     return <h1>loading</h1>;
   } else {
-    // console.log(blogPost.title.rendered);
     return (
       <Container>
         <Row className="blogContent">
-          <Col xs={12} md={8}>
+          <Col xs={12} md={12}>
             <a href="./">
               <h5 className="backToBlogBtn">
                 <span>&#8592;</span>Back to blog
               </h5>
             </a>
-
-            <NavLink to={`/blog/${props.id}`}>
-              {/* <img className="blogImg" alt="blog img" src={blogPost.jetpack_featured_media_url} /> */}
-            </NavLink>
-
-            {/* <title className="singleBlogTitle">Women and health</title>
-          <h3 className="author">By SafePlace October 10, 2020</h3> */}
             <div
               className="article"
               dangerouslySetInnerHTML={{
@@ -55,27 +45,6 @@ export default function SingleBlog(props) {
               <TwitterIcon />
               <LinkedInIcon />
               <InstagramIcon />
-            </div>
-          </Col>
-          <Col md={4} className="relatedPosts">
-            {/* <h3>Related articles:</h3>
-            <div>{blogPost._jetpack_related_posts.map((relatedPosts) => {
-            return (
-            <h6>{relatedPosts.title}</h5>
-            )
-          })}</div> */}
-            <h5>Related articles:</h5>
-            <div className="firstRelatedPost">
-              <h6 className="relatedPosttitle">post 1</h6>
-              <img className="relatedPostImg" alt="blog img" />
-            </div>
-            <div className="secondRelatedPost">
-              <h6 className="relatedPosttitle">post 2</h6>
-              <img className="relatedPostImg" alt="blog img" />
-            </div>
-            <div className="thirdRelatedPost">
-              <h6 className="relatedPosttitle">post 3</h6>
-              <img className="relatedPostImg" alt="blog img" />
             </div>
           </Col>
         </Row>
