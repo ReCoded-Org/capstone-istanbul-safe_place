293bf78b7007e52abda9574ede7276b97481d8e8
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-19 18:56:00 +0300

    work on test

--- src/components/Blogs/index.test.jsx
+++ src/components/Blogs/index.test.jsx
@@ -2,17 +2,66 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Blogs from "./index";
 
-const mockData = {
-  title: {
-    rendered: "Women standing together",
-  },
-  jetpack_featured_media_url:
-    "https://safeplace102505649.files.wordpress.com/2020/11/womentogether.jpg",
-};
+// fetch = jest.fn(() =>
+//   Promise.resolve({
+//     json: () =>
+//       Promise.resolve({
+//         title: {
+//           rendered: "Women standing together",
+//         },
+//         jetpack_featured_media_url:
+//           "https://safeplace102505649.files.wordpress.com/2020/11/womentogether.jpg",
+//       }),
 
-describe("Blogs", () => {
-  it("renders correctly and matches the snapshot", () => {
-    const tree = renderer.create(<Blogs mockData={mockData} />).toJSON();
-    expect(tree).toMatchSnapshot();
+//   })
+
+// );
+
+// console.log(fetch);
+
+// describe("Blogs", () => {
+//   it("renders correctly and matches the snapshot", () => {
+//     const tree = renderer.create(<Blogs />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
+
+describe("Fetch", () => {
+  beforeAll(() => {
+    global.fetch = jest.fn().mockImplementation(
+      () =>
+        new Promise((resolve) => {
+          resolve({
+            json: () =>
+              new Promise((resolve) =>
+                resolve({
+                  title: {
+                    rendered: "Women standing together",
+                  },
+                  jetpack_featured_media_url:
+                    "https://safeplace102505649.files.wordpress.com/2020/11/womentogether.jpg",
+                })
+              ),
+          });
+        })
+    );
+  });
+
+  it("should call fetch with url", () => {
+    require("./index");
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=1"
+    );
+  });
+  console.log(global.fetch);
+  describe("Blogs", () => {
+    it("renders correctly and matches the snapshot", () => {
+      const tree = renderer.create(<Blogs />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  afterAll(() => {
+    global.fetch.mockClear();
   });
 });
