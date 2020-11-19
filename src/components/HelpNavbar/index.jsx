665c0a6a12477302c9f2c8ae82c68904be2089c1
Hiba <hiba.mashfij@gmail.com>
2020-11-19 14:34:26 +0300

    added HELP_PHONE_NUMBER

--- src/components/HelpNavbar/index.jsx
+++ src/components/HelpNavbar/index.jsx
@@ -4,15 +4,18 @@ import { useTranslation } from "react-i18next";
 import phoneIcon from "../../images/icons/phone.svg";
 import "./index.scss";
 
+const HELP_PHONE_NUMBER = "+1-340-5558-235";
+
 const HelpNavbar = () => {
   const { t } = useTranslation();
+
   return (
     <Navbar className="helpNavbar">
       <Container>
         <Col>
           <p>{t("helpNavbar.title")}</p>
           <img src={phoneIcon} alt="Phone icon" />
-          <a href="tel:+1-340-5558-235">{t("helpNavbar.phoneNum")}</a>
+          <a href={`tel:${HELP_PHONE_NUMBER}`}>{HELP_PHONE_NUMBER}</a>
         </Col>
       </Container>
     </Navbar>
