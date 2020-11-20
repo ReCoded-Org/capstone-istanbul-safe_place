3d380123e4cbc64e8de93872eddd17bec65c4d17
Jamil Bailony <jBailony@outlook.com>
2020-11-20 22:24:05 +0300

    build the component

--- src/components/profile/UserPasswordForm/index.jsx
+++ src/components/profile/UserPasswordForm/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { Col } from "react-bootstrap";
+import Input from "../../form/SafePlaceInput";
+import "./index.scss";
 
-export default function UserPasswordForm() {
-  return <div>UserPassword</div>;
+export default function UserPasswordForm({register, errors }) {
+  return (
+    <Col xs={12} className="userPasswordSection">
+      <Input
+        type="password"
+        label="Current password"
+        placeholder="Enter your current password"
+        name="currentPasswrod"
+        ref={register()}
+        errors={errors}
+      />
+
+      <Input
+        type="password"
+        label="New password"
+        placeholder="Enter new password"
+        name="newPasswrod"
+        ref={register()}
+        errors={errors}
+      />
+
+      <Input
+        type="password"
+        label="Confirm password"
+        placeholder="Confirm new password"
+        name="confirmPasswrod"
+        ref={register()}
+        errors={errors}
+      />
+    </Col>
+
+  );
 }
