cf49bccb4d2b7b8fb004a97690737840176333ec
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-21 21:09:48 +0300

    conflict solved with the origin, yarn test and fix passed

--- src/components/SingleBlog/index.test.jsx
+++ src/components/SingleBlog/index.test.jsx
@@ -1,15 +1,54 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { render } from "react-dom";
+import { act } from "react-dom/test-utils";
 import SingleBlog from "./index";
 
-it("renders correctly and matches the snapshot", () => {
-  const tree = renderer
-    .create(
-      <Router>
-        <SingleBlog />
-      </Router>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe("Single blog", () => {
+  it("Renders without crashing", async () => {
+    const mockData = {
+      content: {
+        rendered: "You can find safe place for yourself here",
+      },
+      "jetpack-related-posts": [
+        {
+          id: 61,
+          title: "Women and health",
+          img: {
+            src:
+              "https://safeplace102505649.files.wordpress.com/2020/11/smilinggirl.jpg?w=350&h=200&crop=1",
+          },
+        },
+      ],
+    };
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      })
+    );
+
+    await act(async () => {
+      render(
+        <Router>
+          <SingleBlog />
+        </Router>,
+        container
+      );
+    });
+
+    expect(container).toMatchSnapshot();
+  });
 });
