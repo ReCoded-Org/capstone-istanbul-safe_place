878827cc00720fe37fac7ec22d3c213efb9e96ba
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-21 15:02:23 +0300

    fix seach for help height and responsiveness

--- src/components/BlogsCarouselHome/index.jsx
+++ src/components/BlogsCarouselHome/index.jsx
@@ -49,6 +49,31 @@ export default function BlogsCarouselHome() {
     arrows: true,
     nextArrow: <SliderArrow leftOrRight={"right"} />,
     prevArrow: <SliderArrow leftOrRight={"left"} />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
