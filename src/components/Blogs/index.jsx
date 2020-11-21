9c1843c29ec4a2d160f71c0d0edcce34a88c3818
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-22 02:43:03 +0300

    fixed test with router

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import SearchBar from "../SearchBar";
 import "./index.scss";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const POSTS_PER_PAGE = 9;
 const BLOG_API_URL = `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=${POSTS_PER_PAGE}`;
@@ -14,7 +14,7 @@ export default function Blogs() {
   const fetchBlogPosts = async () => {
     const data = await fetch(BLOG_API_URL);
     const fetchedBlogPosts = await data.json();
-    setBlogsForSearch(fetchedBlogPosts)
+    setBlogsForSearch(fetchedBlogPosts);
     setBlogPosts(fetchedBlogPosts);
   };
 
@@ -43,21 +43,21 @@ export default function Blogs() {
           return (
             <Col key={blogPost.id} md={4}>
               <Link to={`/blog/${blogPost.id}`}>
-              <Card className="blogPost">
-                <Card.Img
-                  variant="top"
-                  src={blogPost.jetpack_featured_media_url}
-                />
-                <Card.Body>
-                  <Card.Title
-                    className="blogPostTitle"
-                    dangerouslySetInnerHTML={{
-                      __html: blogPost.title.rendered,
-                    }}
+                <Card className="blogPost">
+                  <Card.Img
+                    variant="top"
+                    src={blogPost.jetpack_featured_media_url}
                   />
-                </Card.Body>
-              </Card>
-            </Link>
+                  <Card.Body>
+                    <Card.Title
+                      className="blogPostTitle"
+                      dangerouslySetInnerHTML={{
+                        __html: blogPost.title.rendered,
+                      }}
+                    />
+                  </Card.Body>
+                </Card>
+              </Link>
             </Col>
           );
         })}
