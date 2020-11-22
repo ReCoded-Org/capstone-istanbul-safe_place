4b54823df609b61968e43fff3790ff525abd68d3
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-22 21:21:25 +0300

    add scroll up to job page

--- src/containers/JobsTrainingPage/index.jsx
+++ src/containers/JobsTrainingPage/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import JobsTrainingHeader from "../../components/JobsTrainingHeader";
 import OpportunitiesContent from "../../components/OpportunitiesContent";
 
 const JobsTrainingPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Container>
       <JobsTrainingHeader />
