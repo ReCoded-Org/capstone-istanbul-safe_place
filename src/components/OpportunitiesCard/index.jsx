d67aee8b1c0aee210435647d467e50e8a146df00
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-23 01:01:57 +0300

    fix card responsiveness

--- src/components/OpportunitiesCard/index.jsx
+++ src/components/OpportunitiesCard/index.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import "./index.scss";
 
 export default function OpportunitiesCard(opportunity) {
   const { t } = useTranslation();
   return (
-    <Col className="opportunities">
-      <div className="postDate">{t(opportunity.postingDate)}</div>
-      <div className="title">{t(opportunity.title)}</div>
-      <div className="description">{t(opportunity.description)}</div>
-      <span className="borderLine"></span>
-      <div className="contactsDetails">
-        <div className="address">{t(opportunity.address)}</div>
-        <div className="nameOfOpportunityOwner">{t(opportunity.name)}</div>
-        <div className="phoneNumber">{t(opportunity.phoneNumber)}</div>
-      </div>
-    </Col>
+    <Container>
+      <Col className="opportunities">
+        <div className="postDate">{t(opportunity.postingDate)}</div>
+        <div className="title">{t(opportunity.title)}</div>
+        <div className="description">{t(opportunity.description)}</div>
+        <span className="borderLine"></span>
+        <div className="contactsDetails">
+          <div className="address">{t(opportunity.address)}</div>
+          <div className="nameOfOpportunityOwner">{t(opportunity.name)}</div>
+          <div className="phoneNumber">{t(opportunity.phoneNumber)}</div>
+        </div>
+      </Col>
+    </Container>
   );
 }
