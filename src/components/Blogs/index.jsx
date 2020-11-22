516790f204fe600f1e95c6208d782a85a0873a01
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-11-22 21:18:20 +0300

    translation added from WP

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -3,24 +3,42 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import SearchBar from "../SearchBar";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import i18next from "i18next";
 
-const POSTS_PER_PAGE = 9;
+const POSTS_PER_PAGE = 18;
 const BLOG_API_URL = `https://public-api.wordpress.com/wp/v2/sites/safeplace102505649.wordpress.com/posts?per_page=${POSTS_PER_PAGE}`;
 
 export default function Blogs() {
   const [blogPosts, setBlogPosts] = useState([]);
   const [blogsForSearch, setBlogsForSearch] = useState([]);
-
-  const fetchBlogPosts = async () => {
-    const data = await fetch(BLOG_API_URL);
-    const fetchedBlogPosts = await data.json();
-    setBlogsForSearch(fetchedBlogPosts);
-    setBlogPosts(fetchedBlogPosts);
-  };
+  const [languageRef, setLanguageRef] = useState("10221");
 
   useEffect(() => {
+    const fetchBlogPosts = async () => {
+      const data = await fetch(BLOG_API_URL);
+      const fetchedBlogPosts = await data.json();
+      console.log(fetchedBlogPosts);
+      const filteredPosts = await fetchedBlogPosts.filter((post) => {
+        console.log(post.tags)
+        return post.tags.toString() === languageRef;
+      });
+      setBlogsForSearch(fetchedBlogPosts);
+      setBlogPosts(fetchedBlogPosts);
+      setBlogPosts(filteredPosts);
+    };
+
     fetchBlogPosts();
-  }, []);
+  }, [languageRef]);
+
+  i18next.on("languageChanged", (lng) => {
+    let ref = i18next.translator.language;
+    console.log(ref);
+    if (ref === "en") {
+      setLanguageRef("10221");
+    } else if (ref === "ar") {
+      setLanguageRef("38299");
+    }
+  });
 
   // Search function
   const handleSearch = (e, keyword) => {
