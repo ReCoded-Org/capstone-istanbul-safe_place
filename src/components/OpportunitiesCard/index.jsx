24ed797817aaf2725daccdee2b70287fd1c3201b
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-23 01:16:24 +0300

    remove width and height

--- src/components/OpportunitiesCard/index.jsx
+++ src/components/OpportunitiesCard/index.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { Container, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import "./index.scss";
 
 export default function OpportunitiesCard(opportunity) {
   const { t } = useTranslation();
   return (
-    <Container>
-      <Col className="opportunities">
-        <div className="postDate">{t(opportunity.postingDate)}</div>
-        <div className="title">{t(opportunity.title)}</div>
-        <div className="description">{t(opportunity.description)}</div>
-        <span className="borderLine"></span>
-        <div className="contactsDetails">
-          <div className="address">{t(opportunity.address)}</div>
-          <div className="nameOfOpportunityOwner">{t(opportunity.name)}</div>
-          <div className="phoneNumber">{t(opportunity.phoneNumber)}</div>
-        </div>
-      </Col>
-    </Container>
+    <Col className="opportunities justify-content-center">
+      <div className="postDate">{t(opportunity.postingDate)}</div>
+      <div className="title">{t(opportunity.title)}</div>
+      <div className="description">{t(opportunity.description)}</div>
+      <span className="borderLine"></span>
+      <div className="contactsDetails">
+        <div className="address">{t(opportunity.address)}</div>
+        <div className="nameOfOpportunityOwner">{t(opportunity.name)}</div>
+        <div className="phoneNumber">{t(opportunity.phoneNumber)}</div>
+      </div>
+    </Col>
   );
 }
