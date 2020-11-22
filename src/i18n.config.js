c5a55ff4505571a6900620b6877f086deb44087b
Jamil Bailony <jbailony@outlook.com>
2020-11-22 15:51:07 +0300

    fix direction for Arabic language (#156)
    * cfix direction for Arabic language
    
    * fix braces in if and set html lang attr
    
    Co-authored-by: Louis Li <louisrli@users.noreply.github.com>

--- src/i18n.config.js
+++ src/i18n.config.js
@@ -1,11 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
 import Backend from "i18next-http-backend";
 
-i18n.on("languageChanged", function (lng) {
+function setDirection(direction) {
+  document.body.setAttribute("dir", direction);
+  document.documentElement.setAttribute("lang", i18n.language);
+
+  document.getElementById("root").style.textAlign =
+    direction === "rtl" ? "right" : "left";
+}
+
+i18n.on("languageChanged", (lng) => {
   localStorage.setItem("lng", lng);
+  if (lng === "ar") {
+    setDirection("rtl");
+  } else {
+    setDirection("ltr");
+  }
 });
 
 i18n
@@ -14,7 +26,6 @@ i18n
   .use(initReactI18next)
   .init({
     debug: true,
-    lng: "en",
     fallbackLng: "en",
     whitelist: ["en", "ar"],
     interpolation: {
