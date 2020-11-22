11a58b92865560068efb0213b619e972c290e0a0
jennethydyrova <jennethydyrovaa@gmail.com>
2020-11-23 01:38:00 +0300

    delete unused code

--- src/components/OpportunitiesCard/index.jsx
+++ src/components/OpportunitiesCard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import "./index.scss";
 
