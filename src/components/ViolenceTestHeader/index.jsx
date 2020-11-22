46920824425518867ca18d8d61efe5a02e9f5421
Hiba <hiba.mashfij@gmail.com>
2020-11-22 19:20:20 +0300

    added transaltion

--- src/components/ViolenceTestHeader/index.jsx
+++ src/components/ViolenceTestHeader/index.jsx
@@ -5,12 +5,13 @@ import "./index.scss";
 
 const ViolenceTestHeader = () => {
   const { t } = useTranslation();
+
   return (
     <Row className="testHeader justify-content-md-center">
       <h1>{t("violenceTestPage.header")}</h1>
       <p
         dangerouslySetInnerHTML={{
-          __html: t("home.genderViolenceMain.safePlaceStatement", {
+          __html: t("violenceTestPage.subHeader", {
             link:
               '<a href="https://dvs-snoco.org/what-is-domestic-violence-self-test/"> Domestic Violence Services Snohomish County</a>',
           }),
