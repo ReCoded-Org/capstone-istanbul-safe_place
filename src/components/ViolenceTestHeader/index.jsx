7952aa1ff8b523d7564f436360d264aa4ebdafe7
Hiba <hiba.mashfij@gmail.com>
2020-11-22 19:06:29 +0300

    translated header

--- src/components/ViolenceTestHeader/index.jsx
+++ src/components/ViolenceTestHeader/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Row } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 import "./index.scss";
 
 const ViolenceTestHeader = () => {
+  const { t } = useTranslation();
   return (
     <Row className="testHeader justify-content-md-center">
-      <h1>Self Test</h1>
-      <p>
-        This test is developed by <br />
-        <a href="https://dvs-snoco.org/what-is-domestic-violence-self-test/">
-          Domestic Violence Services Snohomish County
-        </a>
-      </p>
+      <h1>{t("violenceTestPage.header")}</h1>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: t("home.genderViolenceMain.safePlaceStatement", {
+            link:
+              '<a href="https://dvs-snoco.org/what-is-domestic-violence-self-test/"> Domestic Violence Services Snohomish County</a>',
+          }),
+        }}
+      />
     </Row>
   );
 };
