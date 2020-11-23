b744ed2e8d9e35c707db0269d80c0e62ada60fb4
Jamil Bailony <jBailony@outlook.com>
2020-11-23 22:22:11 +0300

    yran fix

--- src/containers/ProfilePage/ToggleButton.jsx
+++ src/containers/ProfilePage/ToggleButton.jsx
@@ -5,17 +5,18 @@ import i18n from "i18next";
 function getArrowClass(isCurrentElementSelected) {
   let arrowClass;
 
-  if(isCurrentElementSelected) {
+  if (isCurrentElementSelected) {
     // if Arabic, make the margin from left
-    if(i18n.language === "ar") {
-      arrowClass = "arrow down-rtl"
-    } else { // or make it default, margin from right
-      arrowClass = "arrow down"
+    if (i18n.language === "ar") {
+      arrowClass = "arrow down-rtl";
+    } else {
+      // or make it default, margin from right
+      arrowClass = "arrow down";
     }
-  } else if(i18n.language === "ar") {
-    arrowClass = "arrow left"
+  } else if (i18n.language === "ar") {
+    arrowClass = "arrow left";
   } else {
-    arrowClass = "arrow right"
+    arrowClass = "arrow right";
   }
 
   return arrowClass;
@@ -24,22 +25,24 @@ function getArrowClass(isCurrentElementSelected) {
 export default function ToggleButton({ children, eventKey, callback }) {
   const currentEventKey = React.useContext(AccordionContext);
   const isCurrentEventKey = currentEventKey === eventKey;
-  const [arrowClass, setArrowClass] = React.useState(getArrowClass(isCurrentEventKey))
+  const [arrowClass, setArrowClass] = React.useState(
+    getArrowClass(isCurrentEventKey)
+  );
 
   const decoratedOnClick = useAccordionToggle(
     eventKey,
     () => callback && callback(eventKey)
   );
 
-    // if language changed, update the arrow direction
-    i18n.on("languageChanged", () => {
-      setArrowClass(getArrowClass(isCurrentEventKey));
-    });
+  // if language changed, update the arrow direction
+  i18n.on("languageChanged", () => {
+    setArrowClass(getArrowClass(isCurrentEventKey));
+  });
 
-    // track the arrow direction
-    React.useEffect(() => {
-      setArrowClass(getArrowClass(isCurrentEventKey));
-    }, [isCurrentEventKey])
+  // track the arrow direction
+  React.useEffect(() => {
+    setArrowClass(getArrowClass(isCurrentEventKey));
+  }, [isCurrentEventKey]);
 
   return (
     <button type="button" onClick={decoratedOnClick} className="sectionTitle">
