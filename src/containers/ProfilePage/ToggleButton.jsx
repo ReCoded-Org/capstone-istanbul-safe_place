a23b6b5235521e0d358844fc2fd14f8113fe1d7a
Jamil Bailony <jBailony@outlook.com>
2020-11-23 22:21:35 +0300

    fix the arrow direction when Arabic chosen

--- src/containers/ProfilePage/ToggleButton.jsx
+++ src/containers/ProfilePage/ToggleButton.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import { useAccordionToggle, AccordionContext } from "react-bootstrap";
+import i18n from "i18next";
 
 function getArrowClass(isCurrentElementSelected) {
-  return isCurrentElementSelected ? "arrow down" : "arrow right";
+  let arrowClass;
+
+  if(isCurrentElementSelected) {
+    // if Arabic, make the margin from left
+    if(i18n.language === "ar") {
+      arrowClass = "arrow down-rtl"
+    } else { // or make it default, margin from right
+      arrowClass = "arrow down"
+    }
+  } else if(i18n.language === "ar") {
+    arrowClass = "arrow left"
+  } else {
+    arrowClass = "arrow right"
+  }
+
+  return arrowClass;
 }
 
 export default function ToggleButton({ children, eventKey, callback }) {
   const currentEventKey = React.useContext(AccordionContext);
+  const isCurrentEventKey = currentEventKey === eventKey;
+  const [arrowClass, setArrowClass] = React.useState(getArrowClass(isCurrentEventKey))
 
   const decoratedOnClick = useAccordionToggle(
     eventKey,
     () => callback && callback(eventKey)
   );
 
-  const isCurrentEventKey = currentEventKey === eventKey;
+    // if language changed, update the arrow direction
+    i18n.on("languageChanged", () => {
+      setArrowClass(getArrowClass(isCurrentEventKey));
+    });
+
+    // track the arrow direction
+    React.useEffect(() => {
+      setArrowClass(getArrowClass(isCurrentEventKey));
+    }, [isCurrentEventKey])
 
   return (
     <button type="button" onClick={decoratedOnClick} className="sectionTitle">
-      <i className={getArrowClass(isCurrentEventKey)} />
+      <i className={arrowClass} />
       {children}
     </button>
   );
